501e139b8bea3a74c00912cb903a703a0bcb5af4
anuwidy <widy@bvbmedia.nl>
2014-04-10 18:08:50 +0700

    PARFUMOUTLET-195 #resolve #time 1h45m

--- scripts/admin_pages/admin_mass_product_updater.php
+++ scripts/admin_pages/admin_mass_product_updater.php
@@ -5,12 +5,42 @@ if (!defined('TYPO3_MODE')) {
 $content='<div class="main-heading"><h2>Mass Product Update</h2></div>';
 if ($this->post) {
 	// update tax rate
-	if (isset($this->post['rules_group_id'])) {
+	if (isset($this->post['rules_group_id']) && !empty($this->post['rules_group_id'])) {
 		$str="update tx_multishop_products set tax_id='".$this->post['rules_group_id']."' where page_uid='".$this->showCatalogFromPage."'";
 		$res=$GLOBALS['TYPO3_DB']->sql_query($str);
 		$content.='<strong>For '.$GLOBALS['TYPO3_DB']->sql_affected_rows().' product(s) the TAX rules group has been changed.</strong><br />';
 	}
 	// update prices
+	if (!empty($this->post['amount'])) {
+		$amount=$this->post['amount'];
+		// convert the dec sign from comma to dot
+		if (strpos($amount, ',')!==false) {
+			$amount=str_replace(',', '.', $amount);
+		}
+		if (isset($this->post['amount_vat']) && $this->post['amount_vat'] > 0) {
+			$sql_get_products="select p.products_id, pt.rate as tax_rate from tx_multishop_products p left join tx_multishop_taxes pt on pt.tax_id = p.tax_id where page_uid='".$this->showCatalogFromPage."'";
+			$qry_get_products=$GLOBALS['TYPO3_DB']->sql_query($sql_get_products);
+			$sql_affected_rows=0;
+			while ($rs_get_products = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($qry_get_products)) {
+				$tax_rate = $rs_get_products['tax_rate'];
+				if ($tax_rate > 0) {
+					$amount=($amount/(100+$tax_rate))*100;
+				}
+				$str="update tx_multishop_products set products_price=(products_price+".$amount.") where products_id = '".$rs_get_products['products_id']."' and page_uid='".$this->showCatalogFromPage."'";
+				$res=$GLOBALS['TYPO3_DB']->sql_query($str);
+				$sql_affected_rows+=$GLOBALS['TYPO3_DB']->sql_affected_rows();
+				$str="update tx_multishop_specials set specials_new_products_price=(specials_new_products_price+".$amount.") where products_id = '".$rs_get_products['products_id']."' and page_uid='".$this->showCatalogFromPage."'";
+				$res=$GLOBALS['TYPO3_DB']->sql_query($str);
+			}
+			$content.='<strong>Price update completed. '.$sql_affected_rows.' products has been updated.</strong><br />';
+		} else {
+			$str="update tx_multishop_products set products_price=(products_price+".$amount.") where page_uid='".$this->showCatalogFromPage."'";
+			$res=$GLOBALS['TYPO3_DB']->sql_query($str);
+			$content.='<strong>Price update completed. '.$GLOBALS['TYPO3_DB']->sql_affected_rows().' products has been updated.</strong><br />';
+			$str="update tx_multishop_specials set specials_new_products_price=(specials_new_products_price+".$amount.") where page_uid='".$this->showCatalogFromPage."'";
+			$res=$GLOBALS['TYPO3_DB']->sql_query($str);
+		}
+	}
 	if ($this->post['percentage']) {
 		$multiply=(100+$this->post['percentage'])/100;
 		$str="update tx_multishop_products set products_price=(products_price*".$multiply.") where page_uid='".$this->showCatalogFromPage."'";
@@ -31,21 +61,25 @@ if ($this->post) {
 	<form class="edit_form"  method="post" action="'.mslib_fe::typolink('', 'tx_multishop_pi1[page_section]='.$this->ms['page']).'" >
 		<div class="account-field">
 			<label for="percentage">Increase / decrease product prices by percentage</label>
-			<input name="percentage" type="text" value="'.$this->post['percentage'].'" size="10" />%. Example: -10 to decrease product prices with 10%, or 10 to increase.
+			<input name="percentage" type="text" value="'.$this->post['percentage'].'" size="10" id="percentage" />%. Example: -10 to decrease product prices with 10%, or 10 to increase.<br/>- OR -<br/>
 		</div>
-<div class="account-field">
-		<label for="rules_group_id">VAT Rate</label>	
-		<select name="rules_group_id"><option value="">skip</option>
-		<option value="0">No VAT</option>
-		';
+		<div class="account-field">
+			<label for="amount">by Amount</label>
+			<input name="amount" id="amount" type="text" value="'.$this->post['amount'].'" size="10" />&nbsp;<input name="amount_vat" id="amount_vat" type="checkbox" value="1" />
+			<label for="amount_vat">Substract/Increase price based on including VAT</label>
+		</div>
+		<div class="account-field">
+			<label for="rules_group_id">VAT Rate</label>
+			<select name="rules_group_id"><option value="">skip</option>
+				<option value="0">No VAT</option>';
 	$str="SELECT * FROM `tx_multishop_tax_rule_groups`";
 	$qry=$GLOBALS['TYPO3_DB']->sql_query($str);
 	while (($row=$GLOBALS['TYPO3_DB']->sql_fetch_assoc($qry))!=false) {
 		$content.='<option value="'.$row['rules_group_id'].'">'.htmlspecialchars($row['name']).'</option>';
 	}
 	$content.='
-		</select>
-		</div>			
+			</select>
+		</div>
 		<div class="account-field">
 			<label for="">&nbsp;</label>
 			<input name="Submit" type="submit" value="Update All Products" onclick="return confirm(\'Are you sure?\')" class="msadmin_button" />
