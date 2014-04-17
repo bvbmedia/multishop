bb26fd0f7894bbdf467a27f55880774a77153b3b
anuwidy <widy@bvbmedia.nl>
2014-04-18 01:05:41 +0700

    compare database bugfix and admin_edit product adjustment for custom products relative types

--- pi1/classes/class.mslib_fe.php
+++ pi1/classes/class.mslib_fe.php
@@ -183,7 +183,7 @@ class mslib_fe {
 				break;
 			case 'relatives':
 				//$GLOBALS['TYPO3_DB']->store_lastBuiltQuery=1;
-				$data=$GLOBALS['TYPO3_DB']->exec_SELECTgetRows('products_id,relative_product_id', 'tx_multishop_products_to_relative_products', "(products_id = '".$product['products_id']."' or relative_product_id = '".$product['products_id']."')", '', '',$limit);
+				$data=$GLOBALS['TYPO3_DB']->exec_SELECTgetRows('products_id,relative_product_id', 'tx_multishop_products_to_relative_products', "(products_id = '".$product['products_id']."' or relative_product_id = '".$product['products_id']."') and relation_types='cross-sell'", '', '',$limit);
 				//echo $GLOBALS['TYPO3_DB']->debug_lastBuiltQuery;
 				//die();
 				$product_ids=array();
