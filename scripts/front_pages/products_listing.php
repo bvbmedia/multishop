e685754cc611721cfb80f08974211c888172d1d5
Bas van Beek <bvbmedia@gmail.com>
2014-05-08 20:40:05 +0200

    Fixed query bug for hot products.

--- scripts/front_pages/products_listing.php
+++ scripts/front_pages/products_listing.php
@@ -305,4 +305,5 @@ if (is_array($output_array['meta'])) {
 	$GLOBALS['TSFE']->additionalHeaderData=array_merge($GLOBALS['TSFE']->additionalHeaderData, $output_array['meta']);
 	unset($output_array);
 }
+
 ?>
