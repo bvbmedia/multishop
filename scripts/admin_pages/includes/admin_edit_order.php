dde0154954c596d3cbfbc9a5b1f51115d955155d
Bas van Beek <bvbmedia@gmail.com>
2014-06-08 10:47:23 +0200

    Stripped highslide popup from admin edit customers and edit orders.

--- scripts/admin_pages/includes/admin_edit_order.php
+++ scripts/admin_pages/includes/admin_edit_order.php
@@ -515,13 +515,20 @@ if (is_numeric($this->get['orders_id'])) {
 			}
 		}
 		if ($close_window) {
-			echo $this->pi_getLL('update_processed').'.';
-			echo '
-			<script>
-			window.parent.location.href = \''.$this->FULL_HTTP_URL.mslib_fe::typolink($this->shop_pid.',2003', 'tx_multishop_pi1[page_section]=admin_orders').'\';
-			</script>
-			';
-			exit();
+			if ($this->post['tx_multishop_pi1']['referrer']) {
+				header("Location: ".$this->post['tx_multishop_pi1']['referrer']);
+				exit();
+			} else {
+				header("Location: ".$this->FULL_HTTP_URL.mslib_fe::typolink($this->shop_pid.',2003', 'tx_multishop_pi1[page_section]=admin_orders',1));
+				exit();
+			}
+			if ($this->post['tx_multishop_pi1']['referrer']) {
+				header("Location: ".$this->post['tx_multishop_pi1']['referrer']);
+				exit();
+			} else {
+				header("Location: ".$this->FULL_HTTP_URL.mslib_fe::typolink($this->shop_pid.',2003', 'tx_multishop_pi1[page_section]=admin_orders',1));
+				exit();
+			}
 		}
 		$save_block='
 			<div class="save_block">
@@ -2210,10 +2217,17 @@ $("#button_manual_new_product").click(function(e) {
 		$count++;
 		$content.='<li'.(($count==1) ? ' class="active"' : '').'><a href="#'.$key.'">'.$value[0].'</a></li>';
 	}
+	$subpartArray['###VALUE_REFERRER###']='';
+	if ($this->post['tx_multishop_pi1']['referrer']) {
+		$subpartArray['###VALUE_REFERRER###']=$this->post['tx_multishop_pi1']['referrer'];
+	} else {
+		$subpartArray['###VALUE_REFERRER###']=$_SERVER['HTTP_REFERER'];
+	}
 	$content.='
 		</ul>
 		<div class="tab_container">
-	<form class="admin_product_edit" name="admin_product_edit_'.$product['products_id'].'" id="admin_product_edit_'.$product['products_id'].'" method="post" action="'.mslib_fe::typolink(',2002', '&tx_multishop_pi1[page_section]=admin_ajax&action=edit_order&orders_id='.$_REQUEST['orders_id']).'" enctype="multipart/form-data">
+	<form class="admin_product_edit" name="admin_product_edit_'.$product['products_id'].'" id="admin_product_edit_'.$product['products_id'].'" method="post" action="'.mslib_fe::typolink(',2003', '&tx_multishop_pi1[page_section]=admin_ajax&action=edit_order&orders_id='.$_REQUEST['orders_id']).'" enctype="multipart/form-data">
+		<input type="hidden" name="tx_multishop_pi1[referrer]" id="msAdminReferrer" value="'.$subpartArray['###VALUE_REFERRER###'].'" >
 		';
 	$count=0;
 	foreach ($tabs as $key=>$value) {
