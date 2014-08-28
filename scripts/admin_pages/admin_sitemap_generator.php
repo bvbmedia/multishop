5e4a3e51c3083a03e0e2a8ddb212f4ffa3ab6a58
Bas van Beek <bvbmedia@gmail.com>
2014-08-28 14:28:27 +0200

    [improvement] updated sitemap generator.

--- scripts/admin_pages/admin_sitemap_generator.php
+++ scripts/admin_pages/admin_sitemap_generator.php
@@ -11,27 +11,10 @@ $prefix_domain=$this->FULL_HTTP_URL;
 @unlink($log_file);
 set_time_limit(7200);
 ignore_user_abort(true);
-// price filter
-$price_filter=array();
-$price_filter[]='0-10';
-$price_filter[]='10-25';
-$price_filter[]='25-50';
-$price_filter[]='50-100';
-$price_filter[]='100-250';
-$price_filter[]='250-500';
-$price_filter[]='500-1000';
-$price_filter[]='1000-2000';
-$price_filter[]='2000-3000';
-$price_filter[]='> 3000';
 if (!$this->get['skip_categories']) {
 	$links=array();
-	// generic links
 	$links[]=$prefix_domain."\n";
-	$links[]=$prefix_domain.mslib_fe::typolink('', '')."\n";
-	for ($i=0; $i<5; $i++) {
-		$links[]=$prefix_domain.mslib_fe::typolink('', 'tx_multishop_pi1[page_section]=products_search&p='.$i)."\n";
-	}
-	// generic links eof
+	$links[]=$prefix_domain.mslib_fe::typolink($this->shop_pid)."\n";
 	$qry=$GLOBALS['TYPO3_DB']->sql_query("SELECT * from tx_multishop_categories c, tx_multishop_categories_description cd where c.categories_id=cd.categories_id and c.status=1");
 	while (($categories=$GLOBALS['TYPO3_DB']->sql_fetch_assoc($qry))!=false) {
 		$level=0;
@@ -53,9 +36,6 @@ if (!$this->get['skip_categories']) {
 		if (!$row_tmp['total']) {
 			$jobs=array();
 			$jobs[]='&'.$where.'&tx_multishop_pi1[page_section]=products_listing';
-			foreach ($price_filter as $item) {
-				$jobs[]='&tx_multishop_pi1[page_section]=products_search&'.$where.'&price_filter='.urlencode($item);
-			}
 			foreach ($jobs as $key=>$job) {
 				$add=1;
 				for ($i=0; $i<$max_pages; $i++) {
@@ -110,11 +90,8 @@ if (!$this->get['skip_products']) {
 		}
 	}
 }
-//if ($log_content)
-//{
 @unlink($sitemap_file);
 @copy($log_file, $sitemap_file);
 $content.='<div class="main-heading"><h1>'.$this->pi_getLL('admin_label_sitemap_creator').'</h1></div>';
 $content.='<p>'.$this->pi_getLL('admin_label_your_sitemap_has_been_created').'</p>'.$this->pi_getLL('admin_label_you_can_download_it_here').': <a href="'.$sitemap_file_web_path.'" target="_blank">'.$sitemap_file_web_path.'</a>';
-//}
 ?>
