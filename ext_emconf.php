9b1587a59f6308afb2b5308c746540ec6e6f49ce
Bas van Beek <bvbmedia@gmail.com>
2014-10-28 20:31:36 +0100

    [IMPROVEMENT] changed several Plugin dependancies to suggest.

--- ext_emconf.php
+++ ext_emconf.php
@@ -37,19 +37,15 @@ $EM_CONF[$_EXTKEY] = array(
 			'tt_address' => '2.3.4-2.3.99',
 			'static_info_tables' => '0.0.0',
 		),
+		'conflicts' => array(
+			'dbal' => '0.0.0'
+		),
 		'suggests' => array(
 			't3jquery' => '2.7.1-3.9.99',
 			'rzcolorbox_jquery2' => '1.0.0-1.9.99',
 			'phpexcel_service' => '1.7.6-1.7.99',
 		)
-		'conflicts' => array(
-			'dbal' => '0.0.0'
-		),
-		'suggests' => array(
-		),
 	),
-	'_md5_values_when_last_written' => '',
-	'suggests' => array(
-	),
+	'_md5_values_when_last_written' => ''
 );
 ?>
