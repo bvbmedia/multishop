15b8bd1cd63e50e1b95e84f23af0b3e1771ac874
Bas van Beek <bvbmedia@gmail.com>
2014-11-17 15:24:42 +0100

    [BUGFIX] repaired zipUnpack in front-end for TYPO3 6.2.

--- pi1/classes/class.mslib_befe.php
+++ pi1/classes/class.mslib_befe.php
@@ -2439,6 +2439,21 @@ class mslib_befe {
 	 * @return    array        Files unpacked
 	 */
 	public function zipUnpack($file, $overwrite=0) {
+		if (!isset($GLOBALS['TYPO3_CONF_VARS']['BE']['unzip']['unzip']['split_char'])) {
+			$GLOBALS['TYPO3_CONF_VARS']['BE']['unzip']['unzip']['split_char']=':';
+		}
+		if (!isset($GLOBALS['TYPO3_CONF_VARS']['BE']['unzip']['unzip']['pre_lines'])) {
+			$GLOBALS['TYPO3_CONF_VARS']['BE']['unzip']['unzip']['pre_lines']='1';
+		}
+		if (!isset($GLOBALS['TYPO3_CONF_VARS']['BE']['unzip']['unzip']['post_lines'])) {
+			$GLOBALS['TYPO3_CONF_VARS']['BE']['unzip']['unzip']['post_lines']='0';
+		}
+		if (!isset($GLOBALS['TYPO3_CONF_VARS']['BE']['unzip']['unzip']['file_pos'])) {
+			$GLOBALS['TYPO3_CONF_VARS']['BE']['unzip']['unzip']['file_pos']='1';
+		}
+		if (!(isset($GLOBALS['TYPO3_CONF_VARS']['BE']['unzip']['unzip']['split_char']) && isset($GLOBALS['TYPO3_CONF_VARS']['BE']['unzip']['unzip']['pre_lines']) && isset($GLOBALS['TYPO3_CONF_VARS']['BE']['unzip']['unzip']['post_lines']) && isset($GLOBALS['TYPO3_CONF_VARS']['BE']['unzip']['unzip']['file_pos']))) {
+			return array();
+		}
 		if (!(isset($GLOBALS['TYPO3_CONF_VARS']['BE']['unzip']['unzip']['split_char']) && isset($GLOBALS['TYPO3_CONF_VARS']['BE']['unzip']['unzip']['pre_lines']) && isset($GLOBALS['TYPO3_CONF_VARS']['BE']['unzip']['unzip']['post_lines']) && isset($GLOBALS['TYPO3_CONF_VARS']['BE']['unzip']['unzip']['file_pos']))) {
 			return array();
 		}
