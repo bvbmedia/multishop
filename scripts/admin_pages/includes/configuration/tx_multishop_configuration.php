b732188465ab6bd71fcf0c1225618c5af448338a
Anugrah Widya <widy@bvbmedia.nl>
2014-12-30 17:27:48 +0700

    [IMPROVEMENT] add ENABLE_EDIT_ORDER_PRODUCTS_DESCRIPTION_FIELD

--- scripts/admin_pages/includes/configuration/tx_multishop_configuration.php
+++ scripts/admin_pages/includes/configuration/tx_multishop_configuration.php
@@ -1834,6 +1834,17 @@ $records[]=array(
 	'depend_on_configuration_key'=>'',
 	'use_function'=>''
 );
+$records[]=array(
+	'configuration_title'=>'Enable edit order products description field',
+	'configuration_key'=>'ENABLE_EDIT_ORDER_PRODUCTS_DESCRIPTION_FIELD',
+	'configuration_value'=>'0',
+	'description'=>'Enable edit order products description field',
+	'group_id'=>'9',
+	'use_function'=>'',
+	'set_function'=>'tep_cfg_select_option(array(\'0\',\'1\'),',
+	'depend_on_configuration_key'=>'',
+	'use_function'=>''
+);
 // custom hook that can be controlled by third-party plugin
 if (is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/multishop/scripts/admin_pages/includes/configuration/tx_multishop_configuration.php']['addConfigurationRecordsPreHook'])) {
 	$params=array(
