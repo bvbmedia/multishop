2a839cbde8316312f4005bc977ef5b565d234ef0
Anugrah Widya <widy@bvbmedia.nl>
2015-02-23 22:38:30 +0700

    [BUGFIX] remove the item from special sections when the specials price time are expired

--- scripts/front_pages/includes/specials_section_listing/default.php
+++ scripts/front_pages/includes/specials_section_listing/default.php
@@ -18,82 +18,83 @@ $subparts=array();
 $subparts['template']=$this->cObj->getSubpart($template, '###TEMPLATE###');
 $subparts['item']=$this->cObj->getSubpart($subparts['template'], '###ITEM###');
 foreach ($products as $product) {
-	$output=array();
-	if ($product['products_image']) {
-		$output['image']='<img src="'.mslib_befe::getImagePath($product['products_image'], 'products', $this->imageWidth).'">';
-	} else {
-		$output['image']='<div class="no_image"></div>';
-	}
-	if ($product['categories_id']) {
-		// get all cats to generate multilevel fake url
-		$level=0;
-		$cats=mslib_fe::Crumbar($product['categories_id']);
-		$cats=array_reverse($cats);
-		$where='';
-		if (count($cats)>0) {
-			foreach ($cats as $cat) {
-				$where.="categories_id[".$level."]=".$cat['id']."&";
-				$level++;
-			}
-			$where=substr($where, 0, (strlen($where)-1));
-			$where.='&';
-		}
-		// get all cats to generate multilevel fake url eof
-	}
-	$output['link']=mslib_fe::typolink($this->conf['products_detail_page_pid'], '&'.$where.'&products_id='.$product['products_id'].'&tx_multishop_pi1[page_section]=products_detail');
-	$final_price=mslib_fe::final_products_price($product);
-	if (!$this->ms['MODULES']['DB_PRICES_INCLUDE_VAT'] and ($product['tax_rate'] and $this->ms['MODULES']['SHOW_PRICES_INCLUDING_VAT'])) {
-		$old_price=$product['products_price']*(1+$product['tax_rate']);
-	} else {
-		$old_price=$product['products_price'];
-	}
-	$special_section_price='';
-	$current_product=$product;
-	if ($current_product['tax_rate'] and $this->ms['MODULES']['SHOW_PRICES_WITH_AND_WITHOUT_VAT']) {
-		$output['products_price'].='<div class="price_excluding_vat">'.$this->pi_getLL('excluding_vat').' '.mslib_fe::amount2Cents($current_product['final_price']).'</div>';
-	}
-	if ($current_product['products_price']<>$current_product['final_price']) {
-		if (!$this->ms['MODULES']['DB_PRICES_INCLUDE_VAT'] and ($current_product['tax_rate'] and $this->ms['MODULES']['SHOW_PRICES_INCLUDING_VAT'])) {
-			$old_price=$current_product['products_price']*(1+$current_product['tax_rate']);
+	if ($product['products_price']<>$product['final_price']) {
+		$output=array();
+		if ($product['products_image']) {
+			$output['image']='<img src="'.mslib_befe::getImagePath($product['products_image'], 'products', $this->imageWidth).'">';
 		} else {
-			$old_price=$current_product['products_price'];
+			$output['image']='<div class="no_image"></div>';
 		}
-		$output['products_price'].='<div class="old_price">'.mslib_fe::amount2Cents($old_price).'</div><div class="specials_price">'.mslib_fe::amount2Cents($final_price).'</div>';
-	} else {
-		$output['products_price'].='<div class="price">'.mslib_fe::amount2Cents($final_price).'</div>';
-	}
-	$output['special_section_price']=$output['products_price'];
-	/*
-	if ($old_price and $final_price) {
-		$output['special_section_price']='<div class="old_price">'.mslib_fe::amount2Cents($old_price).'</div><div class="specials_price">'.mslib_fe::amount2Cents($final_price).'</div>';
-	} else {
-		$output['special_section_price']='<div class="price">'.mslib_fe::amount2Cents($final_price).'</div>';
-	}
-	*/
-	$output['products_name']=htmlspecialchars($product['products_name']);
-	$markerArray=array();
-	$markerArray['ITEM_DETAILS_PAGE_LINK']=$output['link'];
-	$markerArray['ITEM_PRODUCTS_NAME']=$output['products_name'];
-	$markerArray['ITEM_PRODUCTS_IMAGE']=$output['image'];
-	$markerArray['ITEM_PRODUCTS_PRICE']=$output['special_section_price'];
-	$markerArray['ITEM_PRODUCTS_ID']='';
-	$markerArray['ITEM_LABEL_SHIPPING_COSTS_OVERVIEW']='';
-	if ($this->ms['MODULES']['DISPLAY_SHIPPING_COSTS_ON_PRODUCTS_LISTING_PAGE']) {
-		$markerArray['ITEM_PRODUCTS_ID']=$product['products_id'];
-		$markerArray['ITEM_LABEL_SHIPPING_COSTS_OVERVIEW']=$this->pi_getLL('shipping_costs');
-	}
-	$markerArray['PRODUCTS_SHORTDESCRIPTION']=$product['products_shortdescription'];
-	$markerArray['PRODUCTS_DESCRIPTION']=$product['products_description'];
-	$markerArray['PRODUCTS_ID']=$product['products_id'];
-	$markerArray['PRODUCTS_DETAIL_PAGE_LINK']=$output['link'];
-	if (mslib_fe::ProductHasAttributes($current_product['products_id'])) {
-		$markerArray['PRODUCTS_ADD_TO_CART_BUTTON_LINK']=$output['link'];
-		$button_submit='<a href="'.$output['link'].'" class="ajax_link"><input name="Submit" type="submit" value="'.$this->pi_getLL('add_to_basket').'"/></a>';
-	} else {
-		$markerArray['PRODUCTS_ADD_TO_CART_BUTTON_LINK']=mslib_fe::typolink($this->shop_pid,'&tx_multishop_pi1[page_section]=shopping_cart&tx_multishop_pi1[action]=add_to_cart&products_id='.$product['products_id']);
-		$button_submit='<input name="Submit" type="submit" value="'.$this->pi_getLL('add_to_basket').'"/>';
-	}
-	$markerArray['ITEM_DETAILS_ADD_TO_CART_BUTTON']='
+		if ($product['categories_id']) {
+			// get all cats to generate multilevel fake url
+			$level=0;
+			$cats=mslib_fe::Crumbar($product['categories_id']);
+			$cats=array_reverse($cats);
+			$where='';
+			if (count($cats)>0) {
+				foreach ($cats as $cat) {
+					$where.="categories_id[".$level."]=".$cat['id']."&";
+					$level++;
+				}
+				$where=substr($where, 0, (strlen($where)-1));
+				$where.='&';
+			}
+			// get all cats to generate multilevel fake url eof
+		}
+		$output['link']=mslib_fe::typolink($this->conf['products_detail_page_pid'], '&'.$where.'&products_id='.$product['products_id'].'&tx_multishop_pi1[page_section]=products_detail');
+		$final_price=mslib_fe::final_products_price($product);
+		if (!$this->ms['MODULES']['DB_PRICES_INCLUDE_VAT'] and ($product['tax_rate'] and $this->ms['MODULES']['SHOW_PRICES_INCLUDING_VAT'])) {
+			$old_price=$product['products_price']*(1+$product['tax_rate']);
+		} else {
+			$old_price=$product['products_price'];
+		}
+		$special_section_price='';
+		$current_product=$product;
+		if ($current_product['tax_rate'] and $this->ms['MODULES']['SHOW_PRICES_WITH_AND_WITHOUT_VAT']) {
+			$output['products_price'].='<div class="price_excluding_vat">'.$this->pi_getLL('excluding_vat').' '.mslib_fe::amount2Cents($current_product['final_price']).'</div>';
+		}
+		if ($current_product['products_price']<>$current_product['final_price']) {
+			if (!$this->ms['MODULES']['DB_PRICES_INCLUDE_VAT'] and ($current_product['tax_rate'] and $this->ms['MODULES']['SHOW_PRICES_INCLUDING_VAT'])) {
+				$old_price=$current_product['products_price']*(1+$current_product['tax_rate']);
+			} else {
+				$old_price=$current_product['products_price'];
+			}
+			$output['products_price'].='<div class="old_price">'.mslib_fe::amount2Cents($old_price).'</div><div class="specials_price">'.mslib_fe::amount2Cents($final_price).'</div>';
+		} else {
+			$output['products_price'].='<div class="price">'.mslib_fe::amount2Cents($final_price).'</div>';
+		}
+		$output['special_section_price']=$output['products_price'];
+		/*
+		if ($old_price and $final_price) {
+			$output['special_section_price']='<div class="old_price">'.mslib_fe::amount2Cents($old_price).'</div><div class="specials_price">'.mslib_fe::amount2Cents($final_price).'</div>';
+		} else {
+			$output['special_section_price']='<div class="price">'.mslib_fe::amount2Cents($final_price).'</div>';
+		}
+		*/
+		$output['products_name']=htmlspecialchars($product['products_name']);
+		$markerArray=array();
+		$markerArray['ITEM_DETAILS_PAGE_LINK']=$output['link'];
+		$markerArray['ITEM_PRODUCTS_NAME']=$output['products_name'];
+		$markerArray['ITEM_PRODUCTS_IMAGE']=$output['image'];
+		$markerArray['ITEM_PRODUCTS_PRICE']=$output['special_section_price'];
+		$markerArray['ITEM_PRODUCTS_ID']='';
+		$markerArray['ITEM_LABEL_SHIPPING_COSTS_OVERVIEW']='';
+		if ($this->ms['MODULES']['DISPLAY_SHIPPING_COSTS_ON_PRODUCTS_LISTING_PAGE']) {
+			$markerArray['ITEM_PRODUCTS_ID']=$product['products_id'];
+			$markerArray['ITEM_LABEL_SHIPPING_COSTS_OVERVIEW']=$this->pi_getLL('shipping_costs');
+		}
+		$markerArray['PRODUCTS_SHORTDESCRIPTION']=$product['products_shortdescription'];
+		$markerArray['PRODUCTS_DESCRIPTION']=$product['products_description'];
+		$markerArray['PRODUCTS_ID']=$product['products_id'];
+		$markerArray['PRODUCTS_DETAIL_PAGE_LINK']=$output['link'];
+		if (mslib_fe::ProductHasAttributes($current_product['products_id'])) {
+			$markerArray['PRODUCTS_ADD_TO_CART_BUTTON_LINK']=$output['link'];
+			$button_submit='<a href="'.$output['link'].'" class="ajax_link"><input name="Submit" type="submit" value="'.$this->pi_getLL('add_to_basket').'"/></a>';
+		} else {
+			$markerArray['PRODUCTS_ADD_TO_CART_BUTTON_LINK']=mslib_fe::typolink($this->shop_pid,'&tx_multishop_pi1[page_section]=shopping_cart&tx_multishop_pi1[action]=add_to_cart&products_id='.$product['products_id']);
+			$button_submit='<input name="Submit" type="submit" value="'.$this->pi_getLL('add_to_basket').'"/>';
+		}
+		$markerArray['ITEM_DETAILS_ADD_TO_CART_BUTTON']='
 		<div class="msFrontAddToCartButton">
 			<form action="'.mslib_fe::typolink($this->shop_pid, 'tx_multishop_pi1[page_section]=shopping_cart&products_id='.$product['products_id']).'" method="post">
 				<input type="hidden" name="quantity" value="1" />
@@ -102,37 +103,38 @@ foreach ($products as $product) {
 			</form>
 		</div>
 	';
-	// get the specials id
-	$str=$GLOBALS['TYPO3_DB']->SELECTquery('specials_id', // SELECT ...
-		'tx_multishop_specials', // FROM ...
-		'products_id="'.$product['products_id'].'"', // WHERE...
-		'', // GROUP BY...
-		'', // ORDER BY...
-		'' // LIMIT ...
-	);
-	$qry=$GLOBALS['TYPO3_DB']->sql_query($str);
-	$res=$GLOBALS['TYPO3_DB']->sql_fetch_assoc($qry);
-
-	$markerArray['SPECIALS_SECTIONS_ID']=$res['specials_id'];
-	$markerArray['SPECIALS_SECTIONS_CODE']=$this->section_code;
-	// custom hook that can be controlled by third-party plugin
-	if (is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/multishop/scripts/front_pages/includes/specials_section_listing']['specialsSectionProductsListingHook'])) {
-		$params=array(
-			'markerArray'=>&$markerArray,
-			'product'=>&$product,
-			'output'=>&$output,
+		// get the specials id
+		$str=$GLOBALS['TYPO3_DB']->SELECTquery('specials_id', // SELECT ...
+			'tx_multishop_specials', // FROM ...
+			'products_id="'.$product['products_id'].'"', // WHERE...
+			'', // GROUP BY...
+			'', // ORDER BY...
+			'' // LIMIT ...
 		);
-		foreach ($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/multishop/scripts/front_pages/includes/specials_section_listing']['specialsSectionsProductsListingHook'] as $funcRef) {
-			t3lib_div::callUserFunction($funcRef, $params, $this);
+		$qry=$GLOBALS['TYPO3_DB']->sql_query($str);
+		$res=$GLOBALS['TYPO3_DB']->sql_fetch_assoc($qry);
+
+		$markerArray['SPECIALS_SECTIONS_ID']=$res['specials_id'];
+		$markerArray['SPECIALS_SECTIONS_CODE']=$this->section_code;
+		// custom hook that can be controlled by third-party plugin
+		if (is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/multishop/scripts/front_pages/includes/specials_section_listing']['specialsSectionProductsListingHook'])) {
+			$params=array(
+				'markerArray'=>&$markerArray,
+				'product'=>&$product,
+				'output'=>&$output,
+			);
+			foreach ($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/multishop/scripts/front_pages/includes/specials_section_listing']['specialsSectionsProductsListingHook'] as $funcRef) {
+				t3lib_div::callUserFunction($funcRef, $params, $this);
+			}
 		}
+		// custom hook that can be controlled by third-party plugin eof
+		if (!$this->ms['MODULES']['DISPLAY_SHIPPING_COSTS_ON_PRODUCTS_LISTING_PAGE']) {
+			$subpartHeaderArray=array();
+			$subpartHeaderArray['###ITEM_SHIPPING_COSTS_OVERVIEW_RELATIVE_WRAPPER###']='';
+			$subparts['item']=$this->cObj->substituteMarkerArrayCached($subparts['item'], array(), $subpartHeaderArray);
+		}
+		$contentItem.=$this->cObj->substituteMarkerArray($subparts['item'], $markerArray, '###|###');
 	}
-	// custom hook that can be controlled by third-party plugin eof
-	if (!$this->ms['MODULES']['DISPLAY_SHIPPING_COSTS_ON_PRODUCTS_LISTING_PAGE']) {
-		$subpartHeaderArray=array();
-		$subpartHeaderArray['###ITEM_SHIPPING_COSTS_OVERVIEW_RELATIVE_WRAPPER###']='';
-		$subparts['item']=$this->cObj->substituteMarkerArrayCached($subparts['item'], array(), $subpartHeaderArray);
-	}
-	$contentItem.=$this->cObj->substituteMarkerArray($subparts['item'], $markerArray, '###|###');
 }
 $subpartArray=array();
 $subpartArray['###ITEM###']=$contentItem;
