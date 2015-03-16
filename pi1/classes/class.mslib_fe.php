6dd51218457e608dfd544e68df2f1c6ae605ae09
Bas van Beek <bvbmedia@gmail.com>
2015-03-16 18:59:26 +0100

    [IMPROVEMENT] added ORDER_UNIT_NAME marker to products_detail and products_listing.

--- pi1/classes/class.mslib_fe.php
+++ pi1/classes/class.mslib_fe.php
@@ -2422,6 +2422,7 @@ class mslib_fe {
 			$required_cols[]='p.minimum_quantity';
 			$required_cols[]='p.maximum_quantity';
 			$required_cols[]='p.products_multiplication';
+			$required_cols[]='oud.name as order_unit_name';
 
 			if ($this->ms['MODULES']['INCLUDE_PRODUCTS_DESCRIPTION_DB_FIELD_IN_PRODUCTS_LISTING']) {
 				$required_cols[]='pd.products_description';
@@ -2451,7 +2452,7 @@ class mslib_fe {
 					t3lib_div::callUserFunction($funcRef, $params, $this);
 				}
 			}
-			$from_clause='tx_multishop_products p left join tx_multishop_specials s on p.products_id = s.products_id left join tx_multishop_manufacturers m on p.manufacturers_id = m.manufacturers_id';
+			$from_clause='tx_multishop_products p left join tx_multishop_specials s on p.products_id = s.products_id left join tx_multishop_manufacturers m on p.manufacturers_id = m.manufacturers_id left join tx_multishop_order_units_description oud on p.order_unit_id=oud.order_unit_id and oud.language_id='.$this->sys_language_uid;
 			if (count($extra_join)) {
 				$from_clause.=" ";
 				$from_clause.=implode(" ", $extra_join);
