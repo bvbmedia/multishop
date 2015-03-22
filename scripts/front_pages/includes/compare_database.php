e2bac9f9f7e0c75936df10556cc64edd98be9a17
Bas van Beek <bvbmedia@gmail.com>
2015-03-22 14:00:14 +0100

    [IMPROVEMENT] added index to endtime in products table to improve query speed.

--- scripts/front_pages/includes/compare_database.php
+++ scripts/front_pages/includes/compare_database.php
@@ -492,6 +492,22 @@ if (!$skipMultishopUpdates) {
 			$messages[]=$str;
 		}
 	}
+	$required_indexes=array();
+	$required_indexes[]='endtime';
+	$indexes=array();
+	$table_name='tx_multishop_products';
+	$str="show indexes from `".$table_name."` ";
+	$qry=$GLOBALS['TYPO3_DB']->sql_query($str);
+	while (($rs=$GLOBALS['TYPO3_DB']->sql_fetch_assoc($qry))!=false) {
+		$indexes[]=$rs['Key_name'];
+	}
+	foreach ($required_indexes as $required_index) {
+		if (!in_array($required_index, $indexes)) {
+			$str="ALTER TABLE  `".$table_name."` ADD KEY `".$required_index."` (`".$required_index."`)";
+			$qry=$GLOBALS['TYPO3_DB']->sql_query($str);
+			$messages[]=$str;
+		}
+	}
 	$str="describe `tx_multishop_products`";
 	$qry=$GLOBALS['TYPO3_DB']->sql_query($str);
 	while (($row=$GLOBALS['TYPO3_DB']->sql_fetch_assoc($qry))!=false) {
