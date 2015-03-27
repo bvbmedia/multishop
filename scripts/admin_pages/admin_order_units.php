ab9357e4dba85ada184451c8c9f1244330223e77
Anugrah Widya <widy@bvbmedia.nl>
2015-03-27 23:26:45 +0700

    [IMPROVEMENT] order unit edit mode

--- scripts/admin_pages/admin_order_units.php
+++ scripts/admin_pages/admin_order_units.php
@@ -10,34 +10,62 @@ if ($this->get['tx_multishop_pi1']['action']) {
 				$res=$GLOBALS['TYPO3_DB']->sql_query($query);
 				$query=$GLOBALS['TYPO3_DB']->DELETEquery('tx_multishop_order_units_description', 'id=\''.$this->get['tx_multishop_pi1']['order_unit_id'].'\'');
 				$res=$GLOBALS['TYPO3_DB']->sql_query($query);
+				header('Location: ' . $this->FULL_HTTP_URL.mslib_fe::typolink($this->shop_pid.',2003', 'tx_multishop_pi1[page_section]=admin_order_units'));
 			}
 			break;
 	}
 }
 if ($this->post) {
-	// add new order status eof
-	if (count($this->post['tx_multishop_pi1']['order_unit_name'])) {
-		if ($this->post['tx_multishop_pi1']['order_unit_name'][0]) {
-			$insertArray=array();
-			$insertArray['code']=$this->post['tx_multishop_pi1']['order_unit_code'];
-			$insertArray['page_uid']=$this->shop_pid;
-			$insertArray['crdate']=time();
-			$query=$GLOBALS['TYPO3_DB']->INSERTquery('tx_multishop_order_units', $insertArray);
+	if (isset($this->post['tx_multishop_pi1']['order_unit_id'])) {
+		$order_unit_id=(int)$this->post['tx_multishop_pi1']['order_unit_id'];
+		if ($order_unit_id) {
+			$updateArray=array();
+			$updateArray['code']=$this->post['tx_multishop_pi1']['order_unit_code'];
+			$query=$GLOBALS['TYPO3_DB']->UPDATEquery('tx_multishop_order_units', 'id=\''.$order_unit_id.'\'', $updateArray);
 			$res=$GLOBALS['TYPO3_DB']->sql_query($query);
-			$id=$GLOBALS['TYPO3_DB']->sql_insert_id();
-			if ($id) {
-				foreach ($this->post['tx_multishop_pi1']['order_unit_name'] as $key=>$value) {
-					$insertArray=array();
-					$insertArray['name']=$value;
-					$insertArray['language_id']=$key;
-					$insertArray['order_unit_id']=$id;
-					$query=$GLOBALS['TYPO3_DB']->INSERTquery('tx_multishop_order_units_description', $insertArray);
-					$res=$GLOBALS['TYPO3_DB']->sql_query($query);
+			// order unit name
+			foreach ($this->post['tx_multishop_pi1']['order_unit_name'] as $key=>$value) {
+				$updateArray=array();
+				$updateArray['name']=$value;
+				$query=$GLOBALS['TYPO3_DB']->UPDATEquery('tx_multishop_order_units_description', 'order_unit_id=\''.$order_unit_id.'\' and language_id = '.$key, $updateArray);
+				$res=$GLOBALS['TYPO3_DB']->sql_query($query);
+			}
+			header('Location: ' . $this->FULL_HTTP_URL.mslib_fe::typolink($this->shop_pid.',2003', 'tx_multishop_pi1[page_section]=admin_order_units'));
+		}
+	} else {
+		// add new order status eof
+		if (count($this->post['tx_multishop_pi1']['order_unit_name'])) {
+			if ($this->post['tx_multishop_pi1']['order_unit_name'][0]) {
+				$insertArray=array();
+				$insertArray['code']=$this->post['tx_multishop_pi1']['order_unit_code'];
+				$insertArray['page_uid']=$this->shop_pid;
+				$insertArray['crdate']=time();
+				$query=$GLOBALS['TYPO3_DB']->INSERTquery('tx_multishop_order_units', $insertArray);
+				$res=$GLOBALS['TYPO3_DB']->sql_query($query);
+				$id=$GLOBALS['TYPO3_DB']->sql_insert_id();
+				if ($id) {
+					foreach ($this->post['tx_multishop_pi1']['order_unit_name'] as $key=>$value) {
+						$insertArray=array();
+						$insertArray['name']=$value;
+						$insertArray['language_id']=$key;
+						$insertArray['order_unit_id']=$id;
+						$query=$GLOBALS['TYPO3_DB']->INSERTquery('tx_multishop_order_units_description', $insertArray);
+						$res=$GLOBALS['TYPO3_DB']->sql_query($query);
+					}
 				}
 			}
+			header('Location: ' . $this->FULL_HTTP_URL.mslib_fe::typolink($this->shop_pid.',2003', 'tx_multishop_pi1[page_section]=admin_order_units'));
 		}
+		// add new order status eof
 	}
-	// add new order status eof
+}
+if ($this->get['tx_multishop_pi1']['action']=='edit') {
+	$str="SELECT o.id, o.code, od.name, od.language_id from tx_multishop_order_units o, tx_multishop_order_units_description od where (o.page_uid='0' or o.page_uid='".$this->shop_pid."') and o.id=od.order_unit_id and od.order_unit_id = ".$this->get['tx_multishop_pi1']['order_unit_id']." order by o.id desc";
+	$qry=$GLOBALS['TYPO3_DB']->sql_query($str);
+	$lngstatus=array();
+	while (($row=$GLOBALS['TYPO3_DB']->sql_fetch_assoc($qry))!=false) {
+		$lngstatus[$row['language_id']]=$row;
+	}
 }
 $content.='<div class="main-heading"><h1>'.$this->pi_getLL('admin_order_unit').'</h1></div>';
 $content.='
@@ -58,24 +86,26 @@ foreach ($this->languages as $key=>$language) {
 			<div class="account-field toggle_advanced_option msEditProductLanguageDivider">
 				<label>'.mslib_befe::strtoupper($this->pi_getLL('language')).'</label>
 				<span><strong>'.$language_lable.'</strong></span>
-			</div>			
+			</div>
 			<div class="account-field">
 				<label for="products_name">'.$this->pi_getLL('admin_name').'</label>
-				<input type="text" class="text" name="tx_multishop_pi1[order_unit_name]['.$language['uid'].']" id="order_unit_name_'.$language['uid'].'" value="'.htmlspecialchars($lngstatus[$language['uid']]['order_unit_name']).'">
-			</div>		
+				<input type="text" class="text" name="tx_multishop_pi1[order_unit_name]['.$language['uid'].']" id="order_unit_name_'.$language['uid'].'" value="'.htmlspecialchars($lngstatus[$language['uid']]['name']).'">
+			</div>
 		';
-}
 $tmpcontent.='
 	<div class="account-field">
 		<label for="order_unit_code">'.$this->pi_getLL('code').'</label>
-		<input type="text" class="text" name="tx_multishop_pi1[order_unit_code]" value="'.htmlspecialchars($lngstatus[$language['uid']]['order_unit_code']).'">
-	</div>		
+		<input type="text" class="text" name="tx_multishop_pi1[order_unit_code]" value="'.htmlspecialchars($lngstatus[$language['uid']]['code']).'">
+	</div>
 	';
+}if ($this->get['tx_multishop_pi1']['action']=='edit') {
+	$tmpcontent.='<input type="hidden" class="text" name="tx_multishop_pi1[order_unit_id]" value="'.$this->get['tx_multishop_pi1']['order_unit_id'].'">';
+}
 $content.=$tmpcontent.'
 <div class="account-field">
 	<label>&nbsp;</label>
 	<input name="Submit" type="submit" value="'.$this->pi_getLL('save').'" class="msadmin_button" />
-</div>	
+</div>
 </fieldset>
 </form>
 ';
@@ -100,14 +130,17 @@ if (count($order_units)) {
 		$content.='<tr class="'.$tr_type.'">
 		<td width="30" align="right">
 			'.$status['id'].'
-		</td>				
+		</td>
 		';
 		$content.='
 		<td>'.$status['code'].'</td>
 		<td>'.$status['name'].'</td>
-		
-		<td width="30" class="align_center">
-			<a href="'.mslib_fe::typolink(',2003', 'tx_multishop_pi1[page_section]='.$this->ms['page'].'&tx_multishop_pi1[order_unit_id]='.$status['id'].'&tx_multishop_pi1[action]=delete').'" onclick="return confirm(\''.$this->pi_getLL('are_you_sure').'?\')" class="admin_menu_remove" alt="'.$this->pi_getLL('delete').'"></a>
+
+		<td width="30" align="center" class="msAdminProductsSearchCellActionIcons">
+			<ul>
+				<li><a href="'.mslib_fe::typolink(',2003', 'tx_multishop_pi1[page_section]='.$this->ms['page'].'&tx_multishop_pi1[order_unit_id]='.$status['id'].'&tx_multishop_pi1[action]=edit').'" class="admin_menu_edit" alt="'.$this->pi_getLL('edit').'"></a></li>
+				<li><a href="'.mslib_fe::typolink(',2003', 'tx_multishop_pi1[page_section]='.$this->ms['page'].'&tx_multishop_pi1[order_unit_id]='.$status['id'].'&tx_multishop_pi1[action]=delete').'" onclick="return confirm(\''.$this->pi_getLL('are_you_sure').'?\')" class="admin_menu_remove" alt="'.$this->pi_getLL('delete').'"></a></li>
+			</ul>
 		</td>';
 		$content.='</tr>';
 	}
