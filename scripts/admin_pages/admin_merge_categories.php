cbb3589c6bf6145e40d9bc8074ccde93df297d12
Anugrah Widya <widy@bvbmedia.nl>
2015-05-28 22:25:24 +0700

    [IMPROVEMENT] delete the source categories when merged

--- scripts/admin_pages/admin_merge_categories.php
+++ scripts/admin_pages/admin_merge_categories.php
@@ -2,73 +2,6 @@
 if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
-$categories=mslib_fe::getSubcatsOnly($this->categoriesStartingPoint, 1);
-//
-$content.='<div class="main-heading"><h1>'.$this->pi_getLL('merge_categories').'</h1></div>
-<form action="'.mslib_fe::typolink($this->shop_pid, 'tx_multishop_pi1[page_section]=merge_categories').'" method="post" class="merge_attribute_options_form">
-	<div class="account-field">
-			<ul>
-			';
-$cat_selectbox='';
-foreach ($categories as $category) {
-	$cat_selectbox.='<option value="'.$category['categories_id'].'" id="sl-cat_'.$category['categories_id'].'">+ '.$category['categories_name'].' (ID: '.$category['categories_id'].')</option>';
-	//
-	$tmp_return_data=array();
-	$tmp_return_data[$category['categories_id']]=$category['categories_name'];
-	//
-	$categories_tree=array();
-	$categories_tree[0][]=array(
-		'id'=>$category['categories_id'],
-		'name'=>$category['categories_name']
-	);
-	mslib_fe::getSubcatsArray($categories_tree,'',$category['categories_id']);
-	//level 0
-	foreach ($categories_tree[$category['categories_id']] as $category_tree_0) {
-		$tmp_return_data[$category_tree_0['id']]=$category_tree_0['name'];
-		if (is_array($categories_tree[$category['categories_id']])) {
-			mslib_fe::build_categories_path($tmp_return_data, $category['categories_id'], $tmp_return_data[$category['categories_id']], $categories_tree, true);
-		}
-	}
-	// build the path
-	/*$content.='<li>';
-	$content.='<input type="checkbox" class="movecats" name="mergecats_source[]" value="'.$category['categories_id'].'">&nbsp;';
-	$content.='<strong>'.$category['categories_name'].' '.(!$category['status'] ? '(disabled)' : '').'</strong>';
-	$content.='</li>'."\n";*/
-	//
-	foreach ($tmp_return_data as $tree_id=>$tree_path) {
-		$tree_path=str_replace('\\', '>', $tree_path);
-		$content.='<li>';
-		$content.='<input type="checkbox" class="movecats" name="mergecats_source[]" value="'.$tree_id.'" id="tree_cats_'.$tree_id.'">&nbsp;';
-		$content.='<label for="tree_cats_'.$tree_id.'">'.$tree_path. ' (ID: '.$tree_id.')' .'</label>';
-		$content.='</li>'."\n";
-	}
-
-	// select box for the target
-	$dataArray=mslib_fe::getSitemap($category['categories_id'], array(), 1, 0);
-	if (count($dataArray)) {
-		$cat_selectbox.=mslib_fe::displayAdminCategories($dataArray, true, 1, $category['categories_id']);
-	}
-}
-$cat_selectbox='<select name="mergecats_target" id="mergecats_target" style="width:400px">
-<option value="0">'.$this->pi_getLL('admin_label_option_main_category').'</option>
-'.$cat_selectbox.'
-</select>
-<script type="text/javascript">
-jQuery(document).ready(function(){
-	jQuery("#mergecats_target").select2();
-});
-</script>
-';
-
-$content.='
-			</ul>
-	</div>
-	<div class="account-field">
-			<label>Merge selected categories to: </label>
-			'.$cat_selectbox.'<input type="submit" id="submit" class="msadmin_button" value="'.$this->pi_getLL('merge_selected').'" />
-	</div>
-</form>
-';
 if ($this->post) {
 	$cat_target_id=$this->post['mergecats_target'];
 	foreach ($this->post['mergecats_source'] as $cat_source_id) {
@@ -125,4 +58,67 @@ if ($this->post) {
 	}
 	header('Location: ' . $this->FULL_HTTP_URL.mslib_fe::typolink($this->shop_pid, 'tx_multishop_pi1[page_section]=merge_categories'));
 }
+//
+$categories=mslib_fe::getSubcatsOnly($this->categoriesStartingPoint, 1);
+//
+$content.='<div class="main-heading"><h1>'.$this->pi_getLL('merge_categories').'</h1></div>
+<form action="'.mslib_fe::typolink($this->shop_pid, 'tx_multishop_pi1[page_section]=merge_categories').'" method="post" class="merge_attribute_options_form">
+	<div class="account-field">
+			<ul>
+			';
+$cat_selectbox='';
+foreach ($categories as $category) {
+	//
+	$tmp_return_data=array();
+	$tmp_return_data[$category['categories_id']]=$category['categories_name'];
+	//
+	$categories_tree=array();
+	$categories_tree[0][]=array(
+		'id'=>$category['categories_id'],
+		'name'=>$category['categories_name']
+	);
+	mslib_fe::getSubcatsArray($categories_tree,'',$category['categories_id']);
+	//level 0
+	foreach ($categories_tree[$category['categories_id']] as $category_tree_0) {
+		$tmp_return_data[$category_tree_0['id']]=$category_tree_0['name'];
+		if (is_array($categories_tree[$category['categories_id']])) {
+			mslib_fe::build_categories_path($tmp_return_data, $category['categories_id'], $tmp_return_data[$category['categories_id']], $categories_tree, true);
+		}
+	}
+	// build the path
+	/*$content.='<li>';
+	$content.='<input type="checkbox" class="movecats" name="mergecats_source[]" value="'.$category['categories_id'].'">&nbsp;';
+	$content.='<strong>'.$category['categories_name'].' '.(!$category['status'] ? '(disabled)' : '').'</strong>';
+	$content.='</li>'."\n";*/
+	//
+	foreach ($tmp_return_data as $tree_id=>$tree_path) {
+		$tree_path=str_replace('\\', '>', $tree_path);
+		$content.='<li>';
+		$content.='<input type="checkbox" class="movecats" name="mergecats_source[]" value="'.$tree_id.'" id="tree_cats_'.$tree_id.'">&nbsp;';
+		$content.='<label for="tree_cats_'.$tree_id.'">'.$tree_path. ' (ID: '.$tree_id.')' .'</label>';
+		$content.='</li>'."\n";
+		//
+		$cat_selectbox.='<option value="'.$tree_id.'" id="sl-cat_'.$tree_id.'">'.$tree_path. ' (ID: '.$tree_id.')' .'</option>';
+	}
+}
+$cat_selectbox='<select name="mergecats_target" id="mergecats_target" style="width:400px">
+<option value="0">'.$this->pi_getLL('admin_label_option_main_category').'</option>
+'.$cat_selectbox.'
+</select>
+<script type="text/javascript">
+jQuery(document).ready(function(){
+	jQuery("#mergecats_target").select2();
+});
+</script>
+';
+
+$content.='
+			</ul>
+	</div>
+	<div class="account-field">
+			<label>Merge selected categories to: </label>
+			'.$cat_selectbox.'<input type="submit" id="submit" class="msadmin_button" value="'.$this->pi_getLL('merge_selected').'" />
+	</div>
+</form>
+';
 ?>
