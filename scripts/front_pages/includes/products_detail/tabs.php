df68b83494058dcf6390cb7c0e523a51136295f4
Anugrah Widya <widy@bvbmedia.nl>
2015-06-17 00:52:15 +0700

    [IMPROVEMENT] make the return data of shippingcosts overview to have all methodes

--- scripts/front_pages/includes/products_detail/tabs.php
+++ scripts/front_pages/includes/products_detail/tabs.php
@@ -426,12 +426,14 @@ if (!$product['products_id']) {
 								shipping_cost_popup+=\'<td class="product_shippingcost_popup_table_center_col">'.$this->pi_getLL('shipping_and_handling_cost_overview').'</td>\';
 								shipping_cost_popup+=\'<td class="product_shippingcost_popup_table_right_col">'.$this->pi_getLL('deliver_by').'</td>\';
 								shipping_cost_popup+=\'</tr>\';
-								$.each(j.shipping_costs_display, function(country_iso_nr, shipping_cost) {
-									shipping_cost_popup+=\'<tr>\';
-									shipping_cost_popup+=\'<td class="product_shippingcost_popup_table_left_col">\' + j.deliver_to[country_iso_nr] + \'</td>\';
-									shipping_cost_popup+=\'<td class="product_shippingcost_popup_table_center_col">\' + shipping_cost + \'</td>\';
-									shipping_cost_popup+=\'<td class="product_shippingcost_popup_table_right_col">\' + j.deliver_by[country_iso_nr] + \'</td>\';
-									shipping_cost_popup+=\'</tr>\';
+								$.each(j.shipping_costs_display, function(shipping_method, shipping_data) {
+									$.each(shipping_data, function(country_iso_nr, shipping_cost) {
+										shipping_cost_popup+=\'<tr>\';
+										shipping_cost_popup+=\'<td class="product_shippingcost_popup_table_left_col">\' + j.deliver_to[shipping_method][country_iso_nr] + \'</td>\';
+										shipping_cost_popup+=\'<td class="product_shippingcost_popup_table_center_col">\' + shipping_cost + \'</td>\';
+										shipping_cost_popup+=\'<td class="product_shippingcost_popup_table_right_col">\' + j.deliver_by[shipping_method][country_iso_nr] + \'</td>\';
+										shipping_cost_popup+=\'</tr>\';
+									});
 								});
 								if (j.delivery_time!=\'e\') {
 									shipping_cost_popup+=\'<tr>\';
