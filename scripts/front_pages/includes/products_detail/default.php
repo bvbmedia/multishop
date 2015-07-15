4deb21c585d89311bd962d91d7801376cdd4dcfb
Bas van Beek <bvbmedia@gmail.com>
2015-07-15 11:27:56 +0200

    [BUGFIX] copied edit/delete product menu to $output array so other plugins can re-use it.

--- scripts/front_pages/includes/products_detail/default.php
+++ scripts/front_pages/includes/products_detail/default.php
@@ -113,8 +113,10 @@ if (!$product['products_id']) {
 	}
 	// products pagination module eof
 	$output['products_name'].=$product['products_name'];
+	$output['admin_link']='';
 	if ($this->ROOTADMIN_USER || ($this->ADMIN_USER && $this->CATALOGADMIN_USER)) {
-		$output['products_name'].='<div class="admin_menu"><a href="'.mslib_fe::typolink($this->shop_pid.',2003', '&tx_multishop_pi1[page_section]=admin_ajax&cid='.$product['categories_id'].'&pid='.$product['products_id'].'&action=edit_product', 1).'" class="admin_menu_edit">Edit</a> <a href="'.mslib_fe::typolink($this->shop_pid.',2003', '&tx_multishop_pi1[page_section]=admin_ajax&cid='.$product['categories_id'].'&pid='.$product['products_id'].'&action=delete_product', 1).'" class="admin_menu_remove" title="Remove"></a></div>';
+		$output['admin_link']='<div class="admin_menu"><a href="'.mslib_fe::typolink($this->shop_pid.',2003', '&tx_multishop_pi1[page_section]=admin_ajax&cid='.$product['categories_id'].'&pid='.$product['products_id'].'&action=edit_product', 1).'" class="admin_menu_edit">Edit</a> <a href="'.mslib_fe::typolink($this->shop_pid.',2003', '&tx_multishop_pi1[page_section]=admin_ajax&cid='.$product['categories_id'].'&pid='.$product['products_id'].'&action=delete_product', 1).'" class="admin_menu_remove" title="Remove"></a></div>';
+		$output['products_name'].=$output['admin_link'];
 	}
 	$final_price=mslib_fe::final_products_price($product);
 	if ($product['tax_id'] && $this->ms['MODULES']['SHOW_PRICES_WITH_AND_WITHOUT_VAT']) {
