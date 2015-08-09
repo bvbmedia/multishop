742ccb6f04084dc1eeb772c6bcffad780918718c
Bas van Beek <bvbmedia@gmail.com>
2015-08-09 17:27:27 +0200

    [TASK] added value type: number_format_2_decimals to admin interface class.

--- pi1/classes/class.tx_mslib_admin_interface.php
+++ pi1/classes/class.tx_mslib_admin_interface.php
@@ -233,6 +233,9 @@ class tx_mslib_admin_interface extends \TYPO3\CMS\Frontend\Plugin\AbstractPlugin
 				foreach ($params['tableColumns'] as $col=>$valArray) {
 					$originalValue=$row[$col];
 					switch ($valArray['valueType']) {
+						case 'number_format_2_decimals':
+							$row[$col]=round(number_format($row[$col], 2), 2);
+							break;
 						case 'number_format_thousand_seperator':
 							$row[$col]=number_format($row[$col], 0, '', '.');
 							break;
