98c8c60cfba37a1ebe57508c664ac4ba966e0f96
Bas van Beek <bvbmedia@gmail.com>
2015-08-18 00:55:12 +0200

    [IMPROVEMENT] tuned visual table alignment in turnover stats per month/year.

--- scripts/admin_pages/includes/admin_stats_orders/turn_over_per_month.php
+++ scripts/admin_pages/includes/admin_stats_orders/turn_over_per_month.php
@@ -452,7 +452,7 @@ for ($i=0; $i<$endDay; $i++) {
 }
 $content.='<table class="table table-striped table-bordered" id="product_import_table">
 <thead><tr>
-	<th width="100" align="right">'.htmlspecialchars($this->pi_getLL('day')).'</th>
+	<th width="200">'.htmlspecialchars($this->pi_getLL('day')).'</th>
 	<th width="100" align="right">'.htmlspecialchars($this->pi_getLL('amount')).'</th>
 	<th width="100" align="right">'.htmlspecialchars($this->pi_getLL('average', 'average')).'</th>
 	<th>'.htmlspecialchars($this->pi_getLL('orders_id')).'</th>
@@ -465,7 +465,7 @@ foreach ($dates as $key=>$value) {
 		$tr_type='even';
 	}
 	$content.='<tr>';
-	$content.='<td align="right">'.$key.'</td>';
+	$content.='<td>'.$key.'</td>';
 	$total_price=0;
 	$system_date=date($selected_year."m-d", $value);
 	if ($search_start_time && $search_end_time) {
