e708264d3c05ab9215bc66ffa105c31af8cbb003
Bas van Beek <bvbmedia@gmail.com>
2015-08-25 11:13:05 +0200

    [TASK] cellname was incorrect in edit_order

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY]=array(
 	'constraints'=>array(
 		'depends'=>array(
 			'php'=>'5.3.15-5.5.99',
-			'typo3'=>'6.2.5-7.3.99',
+			'typo3'=>'6.2.5-7.4.99',
 		),
 		'conflicts'=>array(
 			'dbal'=>'0.0.0'
