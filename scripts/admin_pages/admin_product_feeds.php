6ac2bc73ff62f9d54e0ffdf5e20dd68893fd3b62
Anugrah Widya <widy@bvbmedia.nl>
2015-11-11 16:25:28 +0700

    [IMPROVEMENT] currency in the feed

--- scripts/admin_pages/admin_product_feeds.php
+++ scripts/admin_pages/admin_product_feeds.php
@@ -131,6 +131,7 @@ if ($this->ms['MODULES']['DISPLAY_MANUFACTURERS_ADVICE_PRICE_INPUT']) {
 	$array['manufacturers_advice_price']=$this->pi_getLL('feed_exporter_fields_label_manufacturers_advice_price');
 }
 $array['custom_field']=$this->pi_getLL('feed_exporter_fields_label_custom_field_with_values');
+$array['products_price_currency']=$this->pi_getLL('feed_exporter_fields_label_products_price_currency');
 // attributes
 $str="SELECT * FROM `tx_multishop_products_options` where language_id='".$GLOBALS['TSFE']->sys_language_uid."' order by products_options_id asc";
 $qry=$GLOBALS['TYPO3_DB']->sql_query($str);
