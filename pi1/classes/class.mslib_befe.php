2b468ec51718b6603c0dbaf8ea52f935373ac7d8
Bas van Beek <bvbmedia@gmail.com>
2015-11-17 19:43:11 +0100

    [TASK] added antiXSS method and htmlpurifier library

--- pi1/classes/class.mslib_befe.php
+++ pi1/classes/class.mslib_befe.php
@@ -4385,14 +4385,14 @@ class mslib_befe {
 		require_once(t3lib_extMgm::extPath('multishop').'res/htmlpurifier-4.7.0/HTMLPurifier.auto.php');
 		if(is_array($val)) {
 			foreach($val as $key=>$subVal) {
-				$val[$key]=tx_classifieds::antiXSS($subVal, $mode);
+				$val[$key]=mslib_befe::antiXSS($subVal, $mode);
 			}
 			return $val;
 		} else {
 			$config=HTMLPurifier_Config::createDefault();
 			$config->set('Core.Encoding', 'UTF-8'); // replace with your encoding
 			$config->set('HTML.Doctype', 'XHTML 1.0 Transitional'); // replace with your doctype
-			$config->set('Cache.SerializerPath', $this->DOCUMENT_ROOT.'uploads/tx_classifieds');
+			$config->set('Cache.SerializerPath', $this->DOCUMENT_ROOT.'uploads/tx_multishop');
 			switch($mode) {
 				case 'html':
 					$config->set('HTML.Allowed', 'table,tr,th,td,tbody,thead,tfood,h1[style],h2[style],h3[style],h4[style],h5[style],h6[style],h7[style],style,font[style],iframe[style|frameborder|allowfullscreen|width|height|src],a[href],img[alt|src|unselectable],div,span,p,i,a,b,br,hr,u,strike,strong,em,ul,ol,li,del,ins,strike'); // Allow basic HTML
@@ -4400,13 +4400,13 @@ class mslib_befe {
 					$config->set('HTML.TargetBlank', TRUE);
 					$config->set('HTML.SafeIframe', true);
 					$config->set('URI.SafeIframeRegexp', '%^(//|http://|https://)(www.youtube.com/embed/|player.vimeo.com/video/)%');
-					$config->set('Cache.SerializerPath', $this->DOCUMENT_ROOT.'uploads/tx_classifieds');
+					$config->set('Cache.SerializerPath', $this->DOCUMENT_ROOT.'uploads/tx_multishop');
 					$purifier=new HTMLPurifier($config);
 					return $purifier->purify($val);
 					break;
 				case 'strip_tags':
 					$config->set('HTML.Allowed', ''); // Allow Nothing
-					$config->set('Cache.SerializerPath', $this->DOCUMENT_ROOT.'uploads/tx_classifieds');
+					$config->set('Cache.SerializerPath', $this->DOCUMENT_ROOT.'uploads/tx_multishop');
 					$purifier=new HTMLPurifier($config);
 					return $purifier->purify($val);
 					break;
@@ -4414,7 +4414,7 @@ class mslib_befe {
 					$config->set('HTML.Allowed', 'p,div,span,p,i,a,b,br,hr,u,strike,strong,em,ul,ol,li,del,ins,strike'); // Allow basic HTML
 					$config->set("HTML.Nofollow", TRUE);
 					$config->set('HTML.TargetBlank', TRUE);
-					$config->set('Cache.SerializerPath', $this->DOCUMENT_ROOT.'uploads/tx_classifieds');
+					$config->set('Cache.SerializerPath', $this->DOCUMENT_ROOT.'uploads/tx_multishop');
 					$purifier=new HTMLPurifier($config);
 					return $purifier->purify($val);
 					break;
