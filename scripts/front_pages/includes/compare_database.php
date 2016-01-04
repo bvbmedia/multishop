23cfe586fb5c2237548bfa1c28d9a1f9cedc83e4
Anugrah Widya <widy@bvbmedia.nl>
2016-01-04 22:45:50 +0700

    [IMPROVEMENT] add delivery_state column to tx_multishop_orders table

--- scripts/front_pages/includes/compare_database.php
+++ scripts/front_pages/includes/compare_database.php
@@ -871,6 +871,13 @@ if (!$skipMultishopUpdates) {
 		$qry=$GLOBALS['TYPO3_DB']->sql_query($str);
 		$messages[]=$str;
 	}
+	$str="select delivery_state from tx_multishop_orders limit 1";
+	$qry=$GLOBALS['TYPO3_DB']->sql_query($str);
+	if (!$qry) {
+		$str="ALTER TABLE `tx_multishop_orders` ADD `delivery_state` varchar(150) default '', ADD KEY `delivery_state` (`delivery_state`)";
+		$qry=$GLOBALS['TYPO3_DB']->sql_query($str);
+		$messages[]=$str;
+	}
 	/*
 	// V4 BETA COMPARE DATABASE (MULTIPLE SHOPS DATABASE DESIGN) EOL
 	$str="select tx_multishop_customer_id from fe_users limit 1";
