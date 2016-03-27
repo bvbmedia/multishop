84095842e60724037fe14cb6c706b7f1492cb1b3
Anugrah Widya <widy@bvbmedia.nl>
2016-03-28 00:09:13 +0700

    [BUGFIX] VAT are recalculated again when there is discount which lead to bad VAT value

--- pi1/classes/class.tx_mslib_cart.php
+++ pi1/classes/class.tx_mslib_cart.php
@@ -2793,9 +2793,9 @@ class tx_mslib_cart extends \TYPO3\CMS\Frontend\Plugin\AbstractPlugin {
 				$markerArray['DISCOUNT']=mslib_fe::amount2Cents($this->cart['discount_amount']);
 				$subpartArray['###'.$key.'###']=$this->cObj->substituteMarkerArray($subparts[$key], $markerArray, '###|###');
 				// trick to reduce TAX costs
-				if ($this->ms['MODULES']['SHOW_PRICES_INCLUDING_VAT']) {
-					$this->cart['summarize']['grand_total_vat']=(1-($this->cart['discount_amount']/$this->cart['summarize']['sub_total_including_vat']))*$this->cart['summarize']['grand_total_vat'];
-				}
+				//if ($this->ms['MODULES']['SHOW_PRICES_INCLUDING_VAT']) {
+					//$this->cart['summarize']['grand_total_vat']=(1-($this->cart['discount_amount']/$this->cart['summarize']['sub_total_including_vat']))*$this->cart['summarize']['grand_total_vat'];
+				//}
 			} else {
 				$subpartArray['###'.$key.'###']='';
 				$subpartArray['###NEWSUBTOTAL_WRAPPER###']='';
