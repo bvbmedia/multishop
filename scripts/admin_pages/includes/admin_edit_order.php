c60ee5fd1260c8814c863cefb1867a85c39f59ab
anuwidy <widy@bvbmedia.nl>
2016-08-18 23:12:54 +0700

    [IMPROVEMENT] missing cell

--- scripts/admin_pages/includes/admin_edit_order.php
+++ scripts/admin_pages/includes/admin_edit_order.php
@@ -2191,6 +2191,7 @@ if (is_numeric($this->get['orders_id'])) {
 							}
 							// product final price
 							$order_products_body_data['products_final_price']['value']='';
+                            $order_products_body_data['products_action']['value']='';
 							if ($this->ms['MODULES']['ORDER_EDIT'] and $settings['enable_edit_orders_details']) {
 								$order_products_body_data['products_action']['value']='';
 							}
@@ -2227,9 +2228,7 @@ if (is_numeric($this->get['orders_id'])) {
 							}
 							// product final price
 							$order_products_body_data['products_final_price']['value']='';
-							if ($this->ms['MODULES']['ORDER_EDIT'] and $settings['enable_edit_orders_details']) {
-								$order_products_body_data['products_action']['value']='';
-							}
+                            $order_products_body_data['products_action']['value']='';
 							$order_products_table['body'][$tbody_tag_id]['rows'][]=array(
 								'class'=>$tr_type.' order_products_description',
 								'value'=>$order_products_body_data
