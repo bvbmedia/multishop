a72a38294dfc19f0e1be515f3876dcd6b2e2e064
Bas van Beek <bvbmedia@gmail.com>
2016-09-08 18:50:45 +0200

    [TASK] edit order now only shows paymend condition when it has a value

--- scripts/admin_pages/includes/admin_edit_order.php
+++ scripts/admin_pages/includes/admin_edit_order.php
@@ -1683,10 +1683,12 @@ if (is_numeric($this->get['orders_id'])) {
 				$orderDetailsItem='';
 				$orderDetailsItem='<div class="form-group msAdminEditOrderPaymentConditions">';
 				$orderDetailsItem.='<label class="control-label col-md-3">'.$this->pi_getLL('payment_condition').'</label>';
-				if (!$orders['is_locked']) {
-					$orderDetailsItem.='<div class="col-md-9"><div class="input-group width-fw"><input class="form-control" type="text" name="order_payment_condition" value="'.$orders['payment_condition'].'" /><span class="input-group-addon">'.$this->pi_getLL('days').'</span></div></div>';
-				} else {
-					$orderDetailsItem.='<div class="col-md-9"><p class="form-control-static">'.htmlspecialchars($orders['payment_condition'].' '.$this->pi_getLL('days')).'</p></div>';
+				if ($orders['payment_condition']!='') {
+					if (!$orders['is_locked']) {
+						$orderDetailsItem.='<div class="col-md-9"><div class="input-group width-fw"><input class="form-control" type="text" name="order_payment_condition" value="'.htmlspecialchars($orders['payment_condition']).'" /><span class="input-group-addon">'.$this->pi_getLL('days').'</span></div></div>';
+					} else {
+						$orderDetailsItem.='<div class="col-md-9"><p class="form-control-static">'.htmlspecialchars($orders['payment_condition'].' '.$this->pi_getLL('days')).'</p></div>';
+					}
 				}
 				$orderDetailsItem.='</div><hr>';
 				$orderDetails[]=$orderDetailsItem;
