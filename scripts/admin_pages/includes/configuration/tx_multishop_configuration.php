c02d914d3b701c0a8658911e2f5a0efeccf89942
anuwidy <widy@bvbmedia.nl>
2016-10-21 22:17:12 +0700

    [IMPROVEMENT] add setting to disable the birthdate input in admin add/edit customer

--- scripts/admin_pages/includes/configuration/tx_multishop_configuration.php
+++ scripts/admin_pages/includes/configuration/tx_multishop_configuration.php
@@ -2504,6 +2504,17 @@ $records[]=array(
     'depend_on_configuration_key'=>'',
     'use_function'=>''
 );
+$records[]=array(
+    'configuration_title'=>'Disable birthdate in admin add/edit customer',
+    'configuration_key'=>'DISABLE_BIRTHDATE_IN_ADMIN_CUSTOMER_FORM',
+    'configuration_value'=>'0',
+    'description'=>'Disable birthdate in admin add/edit customer',
+    'group_id'=>'11',
+    'use_function'=>'',
+    'set_function'=>'tep_cfg_select_option(array(\'0\',\'1\'),',
+    'depend_on_configuration_key'=>'',
+    'use_function'=>''
+);
 
 // custom hook that can be controlled by third-party plugin
 if (is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/multishop/scripts/admin_pages/includes/configuration/tx_multishop_configuration.php']['addConfigurationRecordsPreHook'])) {
