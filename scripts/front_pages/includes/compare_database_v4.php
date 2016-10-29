2a0ba2745fc42a8dcf464f0c5816cd8e4aa785ca
Bas van Beek <bvbmedia@gmail.com>
2016-10-29 17:43:00 +0200

    [TASK] added capital price field to orders_products table

--- scripts/front_pages/includes/compare_database_v4.php
+++ scripts/front_pages/includes/compare_database_v4.php
@@ -255,5 +255,11 @@ if (!$qry) {
     $sql_copy="update tx_multishop_orders_products op set products_tax_id = (select tax_id from tx_multishop_products p where p.products_id=op.products_id)";
     $qry=$GLOBALS['TYPO3_DB']->sql_query($sql_copy);
 }
-
+$str="select product_capital_price from tx_multishop_orders_products limit 1";
+$qry=$GLOBALS['TYPO3_DB']->sql_query($str);
+if (!$qry) {
+    $str="ALTER TABLE `tx_multishop_orders_products` ADD `product_capital_price` decimal(24,14) default '0.00000000000000'";
+    $qry=$GLOBALS['TYPO3_DB']->sql_query($str);
+    $messages[]=$str;
+}
 ?>
