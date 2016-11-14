b267f9cfbcc214b22225bf9b440d5f21bcbd4a5c
anuwidy <widy@bvbmedia.nl>
2016-11-14 16:40:55 +0700

    [IMPROVEMENT] always display vendor code in edit order when it's available

--- scripts/admin_pages/includes/admin_edit_order.php
+++ scripts/admin_pages/includes/admin_edit_order.php
@@ -2167,16 +2167,16 @@ if (is_numeric($this->get['orders_id'])) {
 								$row[2].='<img src="'.mslib_befe::getImagePath($product['products_image'], 'products', '50').'">';
 							}
 							$row[2].=$order['products_name'];
+                            if ($this->ms['MODULES']['DISPLAY_SKU_IN_ORDER_DETAILS']=='1' && !empty($product['sku_code'])) {
+                                $row[2].='<br />'.$this->pi_geLL('sku_number').': '.$product['sku_code'];
+                            }
 							if ($this->ms['MODULES']['DISPLAY_PRODUCTS_MODEL_IN_ORDER_DETAILS']=='1' && !empty($order['products_model'])) {
 								$row[2].=' ('.$order['products_model'].')';
 							}
 							if ($this->ms['MODULES']['DISPLAY_EAN_IN_ORDER_DETAILS']=='1' && !empty($product['ean_code'])) {
 								$row[2].='<br />EAN: '.$product['ean_code'];
 							}
-							if ($this->ms['MODULES']['DISPLAY_SKU_IN_ORDER_DETAILS']=='1' && !empty($product['sku_code'])) {
-								$row[2].='<br />SKU: '.$product['sku_code'];
-							}
-							if ($this->ms['MODULES']['DISPLAY_VENDOR_IN_ORDER_DETAILS']=='1' && !empty($product['vendor_code'])) {
+							if (!empty($product['vendor_code'])) {
 								$row[2].='<br />Vendor code: '.$product['vendor_code'];
 							}
 							if ($product['products_id']) {
