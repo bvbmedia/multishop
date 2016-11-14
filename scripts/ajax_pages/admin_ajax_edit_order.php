4c784e5105ac1e5be366069e5b7cab325aacaefc
anuwidy <widy@bvbmedia.nl>
2016-11-14 18:47:37 +0700

    [IMPROVEMENT] new hook

--- scripts/ajax_pages/admin_ajax_edit_order.php
+++ scripts/ajax_pages/admin_ajax_edit_order.php
@@ -297,6 +297,15 @@ switch ($this->get['tx_multishop_pi1']['admin_ajax_edit_order']) {
 		$num_rows=$GLOBALS['TYPO3_DB']->sql_num_rows($qry);
 		if ($num_rows) {
 			while (($row=$GLOBALS['TYPO3_DB']->sql_fetch_assoc($qry))!=false) {
+                //hook to let other plugins further manipulate the replacers
+                if (is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/multishop/scripts/ajax_pages/admin_ajax_edit_order.php']['getProductsIteratorPreProc'])) {
+                    $params = array(
+                        'row' => &$row
+                    );
+                    foreach ($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/multishop/scripts/ajax_pages/admin_ajax_edit_order.php']['getProductsIteratorPreProc'] as $funcRef) {
+                        \TYPO3\CMS\Core\Utility\GeneralUtility::callUserFunction($funcRef, $params, $this);
+                    }
+                }
 				if (!empty($row['products_name'])) {
                     if ($row['products_status']<1) {
 						$row['products_name'].=' [disabled]';
