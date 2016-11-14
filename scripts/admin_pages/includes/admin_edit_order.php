59ac729b179411f724d20d2d000f4d868c18742f
anuwidy <widy@bvbmedia.nl>
2016-11-14 16:55:26 +0700

    [IMPROVEMENT] always display vendor code in edit order when it's available

--- scripts/admin_pages/includes/admin_edit_order.php
+++ scripts/admin_pages/includes/admin_edit_order.php
@@ -2176,7 +2176,7 @@ if (is_numeric($this->get['orders_id'])) {
 							if ($this->ms['MODULES']['DISPLAY_EAN_IN_ORDER_DETAILS']=='1' && !empty($product['ean_code'])) {
 								$row[2].='<br />EAN: '.$product['ean_code'];
 							}
-							if (!empty($product['vendor_code'])) {
+							if (!empty($product['vendor_code']) {
 								$row[2].='<br />Vendor code: '.$product['vendor_code'];
 							}
 							if ($product['products_id']) {
