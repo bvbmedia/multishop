d1a42f57ee5860acdb5ac21a60004b92bbb413ec
anuwidy <widy@bvbmedia.nl>
2016-12-04 19:32:35 +0700

    [BUGFIX] missing obj reference in renderInterface method

--- pi1/classes/class.tx_mslib_admin_interface.php
+++ pi1/classes/class.tx_mslib_admin_interface.php
@@ -543,7 +543,7 @@ class tx_mslib_admin_interface extends \TYPO3\CMS\Frontend\Plugin\AbstractPlugin
                                 'summarize' => &$summarize
                         );
                         foreach ($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/multishop/pi1/classes/class.tx_mslib_admin_interface.php']['tableColumnsPreProc'] as $funcRef) {
-                            \TYPO3\CMS\Core\Utility\GeneralUtility::callUserFunction($funcRef, $conf, $this);
+                            \TYPO3\CMS\Core\Utility\GeneralUtility::callUserFunction($funcRef, $conf, $that);
                         }
                     }
                     $tdClass = array();
