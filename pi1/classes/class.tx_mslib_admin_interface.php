5b76693d195e41a747743f1dfa295b46c1874786
anuwidy <widy@bvbmedia.nl>
2016-12-30 22:52:25 +0700

    [IMPROVEMENT] set limit to 15

--- pi1/classes/class.tx_mslib_admin_interface.php
+++ pi1/classes/class.tx_mslib_admin_interface.php
@@ -161,7 +161,7 @@ class tx_mslib_admin_interface extends \TYPO3\CMS\Frontend\Plugin\AbstractPlugin
             $that->get['display_all_records'] = '';
         }
         if ($that->cookie['limit']) {
-            if (!isset($that->get['limit'])) {
+            if (!isset($that->get['limit']) || $that->get['limit']!=$that->cookie['limit']) {
                 if ($params['settings']['limit'] && is_numeric($params['settings']['limit'])) {
                     $that->get['limit'] = $params['settings']['limit'];
                 } else {
