f52c1122631715bbcac20fde3bf11ed6cfeed995
anuwidy <widy@bvbmedia.nl>
2016-12-30 19:10:07 +0700

    [IMPROVEMENT] set orders to paid when reversal invoice generated

--- pi1/classes/class.mslib_fe.php
+++ pi1/classes/class.mslib_fe.php
@@ -8572,7 +8572,9 @@ class mslib_fe {
                         $query = $GLOBALS['TYPO3_DB']->UPDATEquery('tx_multishop_invoices', 'id=' . $id, $updateArray);
                         $GLOBALS['TYPO3_DB']->sql_query($query);
                         // update orders to paid
-                        $updateArray = array('paid' => 1);
+                        $updateArray=array();
+                        $updateArray['orders_paid_timestamp'] = time();
+                        $updateArray['paid'] = 1;
                         $query = $GLOBALS['TYPO3_DB']->UPDATEquery('tx_multishop_orders', 'orders_id=' . $row['orders_id'], $updateArray);
                         $GLOBALS['TYPO3_DB']->sql_query($query);
                         return 1;
