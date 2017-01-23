112bd2851a223afaf60291812a2fe15b9001a75c
anuwidy <widy@bvbmedia.nl>
2017-01-23 15:55:34 +0700

    [BUGFIX] counter and content amount are not sync

--- scripts/admin_pages/admin_shopping_cart_stats.php
+++ scripts/admin_pages/admin_shopping_cart_stats.php
@@ -117,7 +117,7 @@ foreach ($dates as $key => $value) {
     $system_date = date("Y-m-d", $value);
     $start_time = strtotime($system_date . " 00:00:00");
     $end_time = strtotime($system_date . " 23:59:59");
-    $str = "SELECT c.ip_address,c.session_id FROM tx_multishop_cart_contents c WHERE (" . implode(" AND ", $data_query['where']) . ") and (c.crdate BETWEEN " . $start_time . " and " . $end_time . ") and page_uid='" . $this->shop_pid . "' group by c.session_id ";
+    $str = "SELECT c.ip_address,c.session_id FROM tx_multishop_cart_contents c WHERE (" . implode(" AND ", $data_query['where']) . ") and (c.crdate BETWEEN " . $start_time . " and " . $end_time . ") and page_uid='" . $this->shop_pid . "'";
     $qry = $GLOBALS['TYPO3_DB']->sql_query($str);
     $rows = $GLOBALS['TYPO3_DB']->sql_num_rows($qry);
     $content .= '<td class="text-right">' . number_format($rows) . '</td>';
