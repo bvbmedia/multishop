a29071446d4d87a185aeb09ecf0d561fb8ee1dd2
anuwidy <widy@bvbmedia.nl>
2017-01-23 16:49:18 +0700

    [BUGFIX] counter and content amount are not sync

--- scripts/admin_pages/admin_shopping_cart_stats.php
+++ scripts/admin_pages/admin_shopping_cart_stats.php
@@ -146,7 +146,7 @@ foreach ($dates as $key => $value) {
                     // print customer settings
                     $content .= '<table id="product_import_table" class="table table-striped table-bordered ' . (!$row['is_checkout'] ? 'is_not_checkout' : '') . '">';
                     $tr_rows = array();
-                    $tr_rows[] = '<th class="text-right" width="100">Session ID</th><td>' . $row['session_id'] . '</td>';
+                    //$tr_rows[] = '<th class="text-right" width="100">Session ID</th><td>' . $row['session_id'] . '</td>';
                     $tr_rows[] = '<th class="text-right" width="100">' . $this->pi_getLL('date') . '</th><td>' . strftime("%a. %x %X", $row['crdate']) . '</td>';
                     if ($row['ip_address']) {
                         $tr_rows[] = '<th class="text-right" width="100">' . $this->pi_getLL('ip_address') . '</th><td>' . $row['ip_address'] . '</td>';
