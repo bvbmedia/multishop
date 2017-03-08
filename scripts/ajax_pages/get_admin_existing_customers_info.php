8efb7d66b38c97e143389df6b6fb7bbe457574da
anuwidy <widy@bvbmedia.nl>
2017-03-08 19:20:51 +0700

    [IMPROVEMENT] add ajax server to fetch user info from fe_users table

--- scripts/ajax_pages/get_admin_existing_customers_info.php
+++ scripts/ajax_pages/get_admin_existing_customers_info.php
@@ -44,51 +44,6 @@ if ($this->ADMIN_USER) {
         $res = $GLOBALS['TYPO3_DB']->sql_query($query);
         if ($GLOBALS['TYPO3_DB']->sql_num_rows($res) > 0) {
             while ($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) {
-                unset($row['TSconfig']);
-                unset($row['bic_account_number']);
-                unset($row['cruser_id']);
-                unset($row['disable']);
-                unset($row['disable_jira_worklog_notification_mail']);
-                unset($row['fe_cruser_id']);
-                unset($row['felogin_forgotHash']);
-                unset($row['felogin_redirectPid']);
-                unset($row['http_referer']);
-                unset($row['iban_account_number']);
-                unset($row['ip_address']);
-                unset($row['is_online']);
-                unset($row['jira_worklog_notification_email']);
-                unset($row['lastlogin']);
-                unset($row['lockToDomain']);
-                unset($row['page_uid']);
-                unset($row['password']);
-                unset($row['payment_method']);
-                unset($row['pid']);
-                unset($row['region']);
-                unset($row['starttime']);
-                unset($row['status']);
-                unset($row['tstamp']);
-                unset($row['tx_extbase_type']);
-                unset($row['tx_multishop_changed']);
-                unset($row['tx_multishop_check_subscription']);
-                unset($row['tx_multishop_coc_id']);
-                unset($row['tx_multishop_code']);
-                unset($row['tx_multishop_customer_id']);
-                unset($row['tx_multishop_discount']);
-                unset($row['tx_multishop_geo_lat']);
-                unset($row['tx_multishop_geo_lng']);
-                unset($row['tx_multishop_language']);
-                unset($row['tx_multishop_newsletter']);
-                unset($row['tx_multishop_optin_crdate']);
-                unset($row['tx_multishop_optin_ip']);
-                unset($row['tx_multishop_payment_condition']);
-                unset($row['tx_multishop_quick_checkout']);
-                unset($row['tx_multishop_show_on_googlemap']);
-                unset($row['tx_multishop_source_id']);
-                unset($row['tx_multishop_vat_id']);
-                unset($row['uc']);
-                unset($row['uid']);
-                unset($row['usergroup']);
-                unset($row['username']);
                 $return_data = $row;
             }
         }
