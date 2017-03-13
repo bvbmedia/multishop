b1b5de1acac5e3cabb939b5e68636b887ee2e1e3
Bas van Beek <bvbmedia@gmail.com>
2017-03-13 22:33:21 +0100

    [TASK] Changed the word Month in admin panel notification bar to lowercase.

--- scripts/meta_tags.php
+++ scripts/meta_tags.php
@@ -199,9 +199,9 @@ if ($this->ADMIN_USER) {
         $row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($qry);
         if ($row['total'] > 0) {
             if ($row['total'] == 1) {
-                $string = sprintf($this->pi_getLL('this_s_there_is_one_order_created'), $this->pi_getLL('month'), '<strong>' . number_format($row['total'], 0, '', '.') . '</strong>');
+                $string = sprintf($this->pi_getLL('this_s_there_is_one_order_created'), mslib_befe::strtolower($this->pi_getLL('month')), '<strong>' . number_format($row['total'], 0, '', '.') . '</strong>');
             } else {
-                $string = sprintf($this->pi_getLL('this_s_there_are_s_orders_created'), $this->pi_getLL('month'), '<strong>' . number_format($row['total'], 0, '', '.') . '</strong>');
+                $string = sprintf($this->pi_getLL('this_s_there_are_s_orders_created'), mslib_befe::strtolower($this->pi_getLL('month')), '<strong>' . number_format($row['total'], 0, '', '.') . '</strong>');
             }
             $messages[] = '"<a href=\"' . mslib_fe::typolink($this->shop_pid . ',2003', 'tx_multishop_pi1[page_section]=admin_orders') . '\">' . $string . '</a>"';
         }
