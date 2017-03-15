8ad3621ed33bec4bbee92d0021de3d66c465b704
Bas van Beek <bvbmedia@gmail.com>
2017-03-15 12:46:38 +0100

    [TASK] added font awesome setting to bootstrapPanel method

--- pi1/classes/class.mslib_befe.php
+++ pi1/classes/class.mslib_befe.php
@@ -4828,12 +4828,12 @@ class mslib_befe {
             ), $string);
         }
     }
-    function bootstrapPanel($heading = '', $body = '', $panelClass = 'default', $footer = '', $panelHeadingClass = '', $panelId = '', $enableCollapse = 0, $collapsed = '0', $headingButtons = array()) {
+    function bootstrapPanel($heading = '', $body = '', $panelClass = 'default', $footer = '', $panelHeadingClass = '', $panelId = '', $enableCollapse = 0, $collapsed = '0', $headingButtons = array(), $headingFontAwesomeClass = 'fa fa-file-text-o') {
         if ($enableCollapse) {
             if ($collapsed) {
                 $panelHeadingClasses[] = 'collapsed';
             }
-            $heading = '<a role="button" data-toggle="collapse" href="#' . $panelId . 'Body"><i class="fa fa-file-text-o"></i> ' . $heading . '</a>';
+            $heading = '<a role="button" data-toggle="collapse" href="#' . $panelId . 'Body"><i class="'.$headingFontAwesomeClass.'"></i> ' . $heading . '</a>';
             $panelHeadingParams .= 'data-toggle="collapse" data-target="#' . $panelId . 'Body" aria-expanded="true"';
         }
         $content = '<div' . ($panelId ? ' id="' . $panelId . '"' : '') . ' class="panel panel-' . $panelClass . '">';
