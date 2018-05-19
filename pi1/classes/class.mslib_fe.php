3697f965651004f9ad108c9c3a0ff4d26ec1290b
Bas van Beek <bvbmedia@gmail.com>
2018-05-19 11:06:01 +0200

    [TASK] Added reply_to_email configuration to mailUser method

--- pi1/classes/class.mslib_fe.php
+++ pi1/classes/class.mslib_fe.php
@@ -2319,9 +2319,12 @@ class mslib_fe {
             }
             // try to change URL images to embedded
             $mail->SetFrom($from_email, $from_name);
-            if (!empty($this->ms['MODULES']['STORE_REPLY_TO_EMAIL'])) {
-                $mail->AddReplyTo($this->ms['MODULES']['STORE_REPLY_TO_EMAIL']);
+            if (isset($options['reply_to_email'])) {
+                $mail->AddReplyTo($options['reply_to_email']);
             }
+            /*if (!empty($this->ms['MODULES']['STORE_REPLY_TO_EMAIL'])) {
+                $mail->AddReplyTo($this->ms['MODULES']['STORE_REPLY_TO_EMAIL']);
+            }*/
             if (count($attachments)) {
                 foreach ($attachments as $path) {
                     if ($path and is_file($path)) {
@@ -11035,7 +11038,7 @@ class mslib_fe {
             return true;
         }
     }
-    public function genderSalutation($gender, $custom_salutation='') {
+    public function genderSalutation($gender='', $custom_salutation='') {
         switch ($gender) {
             case '0':
             case 'm':
