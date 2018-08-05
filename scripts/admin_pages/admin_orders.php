00dab838674e07858da372953ea4db70568e4581
anuwidy <widy@bvbmedia.nl>
2018-08-05 18:18:16 +0700

    [IMPROVEMENT] add marker for label and url

--- scripts/admin_pages/admin_orders.php
+++ scripts/admin_pages/admin_orders.php
@@ -1199,6 +1199,11 @@ $subpartArray['###ADMIN_LABEL_TABS_ORDERS###'] = $this->pi_getLL('admin_label_ta
 $subpartArray['###LABEL_RESET_ADVANCED_SEARCH_FILTER###'] = $this->pi_getLL('reset_advanced_search_filter');
 $subpartArray['###ADMIN_LABEL_YES###'] = $this->pi_getLL('yes');
 $subpartArray['###ADMIN_LABEL_NO###'] = $this->pi_getLL('no');
+$subpartArray['###UPDATE_ORDER_STATUS###'] = $this->pi_getLL('update_order_status');
+$subpartArray['###ADMIN_AJAX_UPDATE_ORDER_STATUS_PRE_URL###'] = mslib_fe::typolink($this->shop_pid . ',2002', '&tx_multishop_pi1[page_section]=admin_update_orders_status_pre');
+$subpartArray['###ADMIN_AJAX_UPDATE_ORDER_STATUS_URL###'] = mslib_fe::typolink($this->shop_pid . ',2002', '&tx_multishop_pi1[page_section]=admin_update_orders_status');
+$subpartArray['###ADMIN_AJAX_UPDATE_ORDER_STATUS_URL2###'] = mslib_fe::typolink($this->shop_pid . ',2002', '&tx_multishop_pi1[page_section]=admin_update_orders_status');
+$subpartArray['###LABEL_JS_DO_YOU_WANT_CHANGE_ORDERS_ID_X_TO_STATUS_X###']= $this->pi_getLL('admin_label_js_do_you_want_to_change_orders_id_x_to_status_x');
 // search on shop
 $subpartArray['###SEARCH_IN_SHOP_SELECTBOX###']='';
 if ($this->conf['masterShop']) {
