3b2a54816843deb880f86f74f173fc572b9db62c
anuwidy <widy@bvbmedia.nl>
2018-08-29 23:54:28 +0700

    [BUGFIX] revert to use real product name in printOrdersDetails

--- pi1/classes/class.mslib_befe.php
+++ pi1/classes/class.mslib_befe.php
@@ -4374,7 +4374,11 @@ class mslib_befe {
                 $markerArray['ITEM_ROW_TYPE'] = $tr_type;
                 $markerArray['ITEM_PRODUCT_QTY'] = round($product['qty'], 2);
                 $product_tmp = mslib_fe::getProduct($product['products_id']);
-                $product_name = htmlspecialchars($product['products_name']);
+                if ($product['products_id']>0) {
+                    $product_name = htmlspecialchars($product_tmp['products_name']);
+                } else {
+                    $product_name = htmlspecialchars($product['products_name']);
+                }
                 if ($product['products_article_number']) {
                     $product_name .= ' (' . htmlspecialchars($product['products_article_number']) . ')';
                 }
