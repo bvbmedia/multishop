5f5b29637a7a5fb4dedd22c4ccfc81dab27378b5
Bas van Beek <bas@bvbmedia.nl>
2020-02-24 20:15:42 +0100

    [TASK] added foreign_orders_id to compare database

--- scripts/front_pages/includes/compare_database_v4.php
+++ scripts/front_pages/includes/compare_database_v4.php
@@ -737,4 +737,11 @@ if (!$qry) {
     $qry = $GLOBALS['TYPO3_DB']->sql_query($str);
     $messages[] = $str;
 }
+$str = "select foreign_orders_id from tx_multishop_orders limit 1";
+$qry = $GLOBALS['TYPO3_DB']->sql_query($str);
+if (!$qry) {
+    $str = "ALTER TABLE `tx_multishop_orders` ADD `foreign_orders_id` varchar(30) default ''";
+    $qry = $GLOBALS['TYPO3_DB']->sql_query($str);
+    $messages[] = $str;
+}
 ?>
