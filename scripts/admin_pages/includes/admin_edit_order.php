a24d25ac976d65573489a0fd586154bb9d0b13f4
Anugrah Widya <widy@bvbmedia.nl>
2020-08-02 22:43:44 +0700

    [TASK] BTOYOUBILL-853 update order to paid/unpaid in edit order mode

--- scripts/admin_pages/includes/admin_edit_order.php
+++ scripts/admin_pages/includes/admin_edit_order.php
@@ -1969,91 +1969,6 @@ if (is_numeric($this->get['orders_id'])) {
                 $("#delivery_details_container").html(name + building + delivery_details + "<hr><div class=\"clearfix\"><div class=\"pull-right\"><a href=\"#\" id=\"edit_delivery_info\" class=\"btn btn-primary\"><i class=\"fa fa-pencil\"></i> ' . $this->pi_getLL('edit') . '</a></div></div>");
                 updateCustomerOrderDetails("delivery_details", $("[id^=edit_delivery]").serialize() + "&tx_multishop_pi1[delivery_gender]=" + $(".account-delivery-gender-radio:checked").val());
             });
-            $(document).on("click", ".update_to_paid", function(e){
-                e.preventDefault();
-                var link=$(this).attr("href");
-                var order_id=$(this).attr("data-order-id");
-                var tthis=$(this).parent();
-                jQuery.ajax({
-                    type: "POST",
-                    url: "' . mslib_fe::typolink($this->shop_pid . ',2002', 'tx_multishop_pi1[page_section]=admin_ajax_edit_order&tx_multishop_pi1[admin_ajax_edit_order]=get_order_payment_methods') . '",
-                    dataType: \'json\',
-                    data: "tx_multishop_pi1[order_id]=" + order_id,
-                    success: function(d) {
-                        var tmp_confirm_content =\'' . addslashes(sprintf($this->pi_getLL('admin_label_are_you_sure_that_invoice_x_has_been_paid'), '%order_id%')) . '\';
-                        var confirm_content = \'<div><h3 class="panel-title">\' + tmp_confirm_content . replace(\'%order_id%\', order_id) + \'</h3></div><div class="form-group" id="popup_order_wrapper_listing">\' + d.payment_method_date_purchased + \'</div>\';
-                        var confirm_box=jQuery.confirm({
-                            title: \'\',
-                            content: confirm_content,
-                            columnClass: \'col-md-6 col-md-offset-4 \',
-                            confirm: function(){
-                                var payment_id=this.$b.find("#payment_method_sb_listing").val();
-                                var date_paid=this.$b.find("#orders_paid_timestamp").val();
-                                var send_paid_letter=this.$b.find("#send_payment_received_email").prop("checked") ? 1 : 0;
-                                //
-                                jQuery.ajax({
-                                    type: "POST",
-                                    url: "' . mslib_fe::typolink($this->shop_pid . ',2002', 'tx_multishop_pi1[page_section]=admin_ajax_edit_order&tx_multishop_pi1[admin_ajax_edit_order]=update_paid_status_save_popup_value') . '",
-                                    dataType: \'json\',
-                                    data: "tx_multishop_pi1[payment_id]=" + payment_id + "&tx_multishop_pi1[date_paid]=" + date_paid + "&tx_multishop_pi1[order_id]=" + order_id + "&tx_multishop_pi1[send_paid_letter]=" + send_paid_letter + "&tx_multishop_pi1[action]=update_selected_orders_to_paid",
-                                    success: function(d) {
-                                        if (d.status=="OK") {
-                                            var return_string = \'<a href="#" class="update_to_unpaid" data-order-id="\' + order_id + \'"><span class="admin_status_red disabled" alt="' . $this->pi_getLL('admin_label_disable') . '"></span></a><span class="admin_status_green" alt="' . $this->pi_getLL('admin_label_enable') . '"></span>\';
-                                            tthis.html(return_string);
-                                        }
-                                    }
-                                });
-                                //window.location =link;
-                            },
-                            cancel: function(){},
-                            confirmButton: \'' . $this->pi_getLL('yes') . '\',
-                            cancelButton: \'' . $this->pi_getLL('no') . '\',
-                            backgroundDismiss: false
-                        });
-                        confirm_box.$b.find("#orders_paid_timestamp_visual").datepicker({
-                            dateFormat: "' . $this->pi_getLL('locale_date_format_js', 'yy/mm/dd') . '",
-                            altField: "#orders_paid_timestamp",
-                            altFormat: "yy-mm-dd",
-                            changeMonth: true,
-                            changeYear: true,
-                            showOtherMonths: true,
-                            yearRange: "' . (date("Y") - 15) . ':' . (date("Y") + 2) . '"
-                        });
-                    }
-                });
-            });
-            $(document).on("click", ".update_to_unpaid", function(e){
-                e.preventDefault();
-                var link=$(this).attr("href");
-                var order_id=$(this).attr("data-order-id");
-                var tthis=$(this).parent();
-                var tmp_confirm_content =\'' . addslashes(sprintf($this->pi_getLL('admin_label_are_you_sure_that_invoice_x_has_not_been_paid'), '%order_id%')) . '\';
-                var confirm_content=\'<div class="confirm_to_unpaid_status">\' + tmp_confirm_content.replace(\'%order_id%\', order_id) + \'</div>\';
-                //
-                $.confirm({
-                    title: \'\',
-                    content: confirm_content,
-                    columnClass: \'col-md-6 col-md-offset-4 \',
-                    confirm: function(){
-                        jQuery.ajax({
-                            type: "POST",
-                            url: "' . mslib_fe::typolink($this->shop_pid . ',2002', 'tx_multishop_pi1[page_section]=admin_ajax_edit_order&tx_multishop_pi1[admin_ajax_edit_order]=update_paid_status_save_popup_value') . '",
-                            dataType: \'json\',
-                            data: "tx_multishop_pi1[order_id]=" + order_id + "&tx_multishop_pi1[action]=update_selected_orders_to_not_paid",
-                            success: function(d) {
-                                if (d.status=="OK") {
-                                    var return_string = \'<span class="admin_status_red" alt="' . $this->pi_getLL('admin_label_disable') . '"></span><a href="#" class="update_to_paid" data-order-id="\' + order_id + \'"><span class="admin_status_green disabled" alt="' . $this->pi_getLL('admin_label_enable') . '"></span></a>\';
-                                    tthis.html(return_string);
-                                }
-                            }
-                        });
-                    },
-                    cancel: function(){},
-                    confirmButton: \'Yes\',
-                    cancelButton: \'NO\'
-                });
-            });    
-                
         });
         </script>';
             $GLOBALS['TSFE']->additionalHeaderData[] = $headerData;
@@ -2159,24 +2074,13 @@ if (is_numeric($this->get['orders_id'])) {
 				</div>
             ';
             $paid_status = '';
-            if ($order['is_locked']) {
-                if (!$order['paid']) {
-                    $paid_status .= '<span class="admin_status_red" alt="' . $this->pi_getLL('has_not_been_paid') . '" title="' . $this->pi_getLL('has_not_been_paid') . '"></span> ';
-                    $paid_status .= '<span class="admin_status_green disabled" alt="' . $this->pi_getLL('change_to_paid') . '" title="' . $this->pi_getLL('change_to_paid') . '"></span>';
-                } else {
-                    $paid_status .= '<span class="admin_status_red disabled" alt="' . $this->pi_getLL('change_to_not_paid') . '" title="' . $this->pi_getLL('change_to_not_paid') . '"></span> ';
-                    $paid_status .= '<span class="admin_status_green" alt="' . $this->pi_getLL('has_been_paid') . '" title="' . $this->pi_getLL('has_been_paid') . '"></span>';
-                }
+            if (!$order['paid']) {
+                $paid_status .= '<span class="admin_status_red" alt="' . $this->pi_getLL('has_not_been_paid') . '" title="' . $this->pi_getLL('has_not_been_paid') . '"></span> ';
+                $paid_status .= '<span class="admin_status_green disabled" alt="' . $this->pi_getLL('change_to_paid') . '" title="' . $this->pi_getLL('change_to_paid') . '"></span>';
             } else {
-                if (!$order['paid']) {
-                    $paid_status .= '<span class="admin_status_red" alt="' . $this->pi_getLL('has_not_been_paid') . '" title="' . $this->pi_getLL('has_not_been_paid') . '"></span> ';
-                    $paid_status .= '<a href="' . mslib_fe::typolink($this->shop_pid . ',2003', '&tx_multishop_pi1[page_section]=' . $this->ms['page'] . '&tx_multishop_pi1[action]=update_orders_to_paid&orders_id=' . $order['orders_id']) . '" class="update_to_paid" data-order-id="' . $order['orders_id'] . '"><span class="admin_status_green disabled" alt="' . $this->pi_getLL('change_to_paid') . '" title="' . $this->pi_getLL('change_to_paid') . '"></span></a>';
-                } else {
-                    $paid_status .= '<a href="' . mslib_fe::typolink($this->shop_pid . ',2003', '&tx_multishop_pi1[page_section]=' . $this->ms['page'] . '&tx_multishop_pi1[action]=update_orders_to_not_paid&orders_id=' . $order['orders_id']) . '" class="update_to_unpaid" data-order-id="' . $order['orders_id'] . '"><span class="admin_status_red disabled" alt="' . $this->pi_getLL('change_to_not_paid') . '" title="' . $this->pi_getLL('change_to_not_paid') . '"></span></a> ';
-                    $paid_status .= '<span class="admin_status_green" alt="' . $this->pi_getLL('has_been_paid') . '" title="' . $this->pi_getLL('has_been_paid') . '"></span>';
-                }
+                $paid_status .= '<span class="admin_status_red disabled" alt="' . $this->pi_getLL('change_to_not_paid') . '" title="' . $this->pi_getLL('change_to_not_paid') . '"></span> ';
+                $paid_status .= '<span class="admin_status_green" alt="' . $this->pi_getLL('has_been_paid') . '" title="' . $this->pi_getLL('has_been_paid') . '"></span>';
             }
-
             $orderDetails[] = '
             <div class="form-group">
                 <label class="control-label col-md-3">' . $this->pi_getLL('admin_paid') . '</label>
