83c969858931dc5099832e178b1647fe77b5fa86
Bas van Beek <bas@bvbmedia.nl>
2021-01-10 13:46:26 +0100

    [TASK] STOFFEFTL-195

--- scripts/ajax_pages/update_product_info.php
+++ scripts/ajax_pages/update_product_info.php
@@ -4,10 +4,17 @@ if (!defined('TYPO3_MODE')) {
 }
 $response = array();
 $response['status'] = 'NOTOK';
+$erno = array();
 if (is_numeric($this->post['tx_multishop_pi1']['pid'])) {
     switch ($this->get['tx_multishop_pi1']['update_product_info']) {
         case 'products_price':
-            if (is_float($this->post['tx_multishop_pi1']['value'])) {
+            if ((float)$this->post['tx_multishop_pi1']['value'] == '') {
+                // Error!
+                $erno[] = 'Input is not valid';
+            } else {
+                $this->post['tx_multishop_pi1']['value'] = (float)$this->post['tx_multishop_pi1']['value'];
+            }
+            if (!count($erno)) {
                 if ($this->ms['MODULES']['SHOW_PRICES_INCLUDING_VAT'] == '1') {
                     $product = mslib_befe::getRecord($this->post['tx_multishop_pi1']['pid'], 'tx_multishop_products', 'products_id', array(), 'tax_id');
                     if ($product['tax_id'] > 0) {
@@ -25,7 +32,13 @@ if (is_numeric($this->post['tx_multishop_pi1']['pid'])) {
             }
             break;
         case 'products_quantity':
-            if (is_float($this->post['tx_multishop_pi1']['value'])) {
+            if ((float)$this->post['tx_multishop_pi1']['value'] == '') {
+                // Error!
+                $erno[] = 'Input is not valid';
+            } else {
+                $this->post['tx_multishop_pi1']['value'] = (float)$this->post['tx_multishop_pi1']['value'];
+            }
+            if (!count($erno)) {
                 $updateArray = array();
                 $updateArray['products_quantity'] = $this->post['tx_multishop_pi1']['value'];
                 $query = $GLOBALS['TYPO3_DB']->UPDATEquery('tx_multishop_products', 'products_id=\'' . $this->post['tx_multishop_pi1']['pid'] . '\'', $updateArray);
@@ -35,7 +48,13 @@ if (is_numeric($this->post['tx_multishop_pi1']['pid'])) {
             }
             break;
         case 'products_status':
-            if (is_numeric($this->post['tx_multishop_pi1']['value'])) {
+            if ((float)$this->post['tx_multishop_pi1']['value'] == '') {
+                // Error!
+                $erno[] = 'Input is not valid';
+            } else {
+                $this->post['tx_multishop_pi1']['value'] = (float)$this->post['tx_multishop_pi1']['value'];
+            }
+            if (!count($erno)) {
                 $updateArray = array();
                 $updateArray['products_status'] = $this->post['tx_multishop_pi1']['value'];
                 $query = $GLOBALS['TYPO3_DB']->UPDATEquery('tx_multishop_products', 'products_id=\'' . $this->post['tx_multishop_pi1']['pid'] . '\'', $updateArray);
@@ -61,11 +80,15 @@ if (is_numeric($this->post['tx_multishop_pi1']['pid'])) {
 // custom page hook that can be controlled by third-party plugin
 if (is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/multishop/scripts/ajax_pages/update_product_info.php']['ajaxUpdateProductInfoPostProc'])) {
     $params = array(
-            'response' => &$response
+            'response' => &$response,
+            'erno' => &$erno
     );
     foreach ($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/multishop/scripts/ajax_pages/update_product_info.php']['ajaxUpdateProductInfoPostProc'] as $funcRef) {
         \TYPO3\CMS\Core\Utility\GeneralUtility::callUserFunction($funcRef, $params, $this);
     }
+}
+if (count($erno)) {
+    $response['errors'] = $erno;
 }
 echo json_encode($response);
 exit();
