71462a0cad07d98626cad055349b85b4ed05561d
Bas van Beek <bas@bvbmedia.nl>
2022-10-17 21:59:32 +0200

    LKNLSPR-310 Added flat database support by multishop_flat_catalog plugin

--- pi1/classes/class.mslib_fe.php
+++ pi1/classes/class.mslib_fe.php
@@ -348,7 +348,7 @@ class mslib_fe {
         return $content;
     }
     public function getProductsPageSet($filter = array(), $offset = 0, $limit = 0, $orderby = array(), $having = array(), $select = array(), $where = array(), $redirect_if_one_product = 0, $extra_from = array(), $groupby = array(), $search_section = 'products_search', $select_total_count = '', $returnTotalCountOnly = 0, $enableFetchTaxRate = 1, $extra_join = array(), $includeDisabled = 0, $skipIsDeepest = 0) {
-        if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('multishop_flat_catalog')) {
+        if ($this->get['type'] == '2002' && \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('multishop_flat_catalog')) {
             // Force flat mode
             $this->ms['MODULES']['FLAT_DATABASE'] = 1;
         }
@@ -923,7 +923,7 @@ class mslib_fe {
                 }
             }
         }
-        if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('multishop_flat_catalog') && $this->ms['MODULES']['FLAT_DATABASE']) {
+        if ($this->get['type'] == '2002' && \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('multishop_flat_catalog')) {
             // Force revert enable flat mode
             $this->ms['MODULES']['FLAT_DATABASE'] = 0;
         }
